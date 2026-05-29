8189bfe113e863529319274a75120e73f94b490a
Krishnveer Bhagod <112793546+K3-github@users.noreply.github.com>
2026-05-29 18:26:50 +0530

    Time: 10 ms (30.21%), Space: 58.1 MB (63.91%) - LeetHub

---
+++ 1020-number-of-enclaves/1020-number-of-enclaves.java
@@ -0,0 +1,66 @@
+class Pair{
+    int first;
+    int second;
+
+    Pair(int first,int second){
+        this.first=first;
+        this.second=second;
+    }
+};
+class Solution {
+    public int numEnclaves(int[][] b) {
+        int n=b.length;
+        int m=b[0].length;
+
+        Queue<Pair> q=new LinkedList<>();
+        boolean[][] vis=new boolean[n][m];
+        for(int i=0;i<n;i++){
+            if(b[i][0]==1){
+                q.offer(new Pair(i,0));
+                vis[i][0]=true;
+            }
+            if(b[i][m-1]==1){
+                q.offer(new Pair(i,m-1));
+                vis[i][m-1]=true;
+            }
+        }
+        for(int j=1;j<m-1;j++){
+            if(b[0][j]==1){
+                q.offer(new Pair(0,j));
+                vis[0][j]=true;
+            }
+            if(b[n-1][j]==1){
+                q.offer(new Pair(n-1,j));
+                vis[n-1][j]=true;
+            }
+        }
+
+        while(!q.isEmpty()){
+            Pair p=q.poll();
+            int row=p.first;
+            int col=p.second;
+
+            int[] drow=new int[]{-1,0,+1,0};
+            int[] dcol=new int[]{0,+1,0,-1};
+
+            for(int d=0;d<4;d++){
+                int nrow=row+drow[d];
+                int ncol=col+dcol[d];
+
+                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && b[nrow][ncol]==1 && !vis[nrow][ncol]){
+                    q.offer(new Pair(nrow,ncol));
+                    vis[nrow][ncol]=true;
+                }
+            }
+        }
+        int count=0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(b[i][j]==1 && !vis[i][j]){
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+}
