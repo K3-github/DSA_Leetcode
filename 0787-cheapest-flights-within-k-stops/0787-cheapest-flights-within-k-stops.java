e4e911316becc45de131e30cebc3bfd845b37fe6
Krishnveer Bhagod <112793546+K3-github@users.noreply.github.com>
2026-06-01 00:03:24 +0530

    Time: 7 ms (46.07%), Space: 46.6 MB (38.64%) - LeetHub

---
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.java
@@ -0,0 +1,43 @@
+class Pair{
+    int first;
+    int second;
+
+    Pair(int first,int second){
+        this.first=first;
+        this.second=second;
+    }
+};
+class Solution {
+    public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
+        List<List<Pair>> graph=new ArrayList<>();
+        for(int i=0;i<n;i++){
+            graph.add(new ArrayList<>());
+        }
+        for(int[] f: flights){
+            graph.get(f[0]).add(new Pair(f[1],f[2]));
+        }
+
+        PriorityQueue<int[]> q=new PriorityQueue<>((a,b)-> a[0] - b[0]);
+        q.offer(new int[]{0,src,0});
+        int[] dist=new int[n];
+        for(int i=0;i<n;i++) dist[i]=Integer.MAX_VALUE;
+        dist[src]=0;
+
+        while(!q.isEmpty()){
+            int[] tp=q.poll();
+
+            int stops=tp[0],currNode=tp[1],currPrice=tp[2];
+
+            if(stops>k) continue;
+            for(Pair p: graph.get(currNode)){
+                int adjNode=p.first;
+                int reqPrice=p.second;
+                if(currPrice+reqPrice<dist[adjNode]){
+                    dist[adjNode]=currPrice+reqPrice;
+                    q.offer(new int[]{stops+1,adjNode,dist[adjNode]});
+                }
+            }
+        }
+        return dist[dst]==2147483647 ? -1: dist[dst];
+    }
+}
