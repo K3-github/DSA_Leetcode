8ffabcb78c04169e7acfec7e7abfdb5e2ed5ce01
Krishnveer Bhagod <112793546+K3-github@users.noreply.github.com>
2026-06-04 11:53:51 +0530

    Time: 0 ms (100%), Space: 41.8 MB (92.87%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,16 +1,10 @@
 class Solution {
-    private int solve(int ind,int n,int[] dp){
-        if(ind==n) return 1;
-        if(ind>n) return 0;
-        if(dp[ind]!=-1) return dp[ind];
-        int w1=solve(ind+1,n,dp);
-        int w2=solve(ind+2,n,dp);
-
-        return dp[ind]=w1+w2;
-    }
     public int climbStairs(int n) {
         int[] dp=new int[n+1];
-        for(int i=0;i<=n;i++) dp[i]=-1;
-        return solve(0,n,dp);
+        dp[0]=1;dp[1]=1;
+        for(int i=2;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n];
     }
 }
