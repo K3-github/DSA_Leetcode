c180903c9c75f4de2425244af44700b96dcaacbf
Krishnveer Bhagod <112793546+K3-github@users.noreply.github.com>
2026-06-04 16:58:52 +0530

    Time: 0 ms (100%), Space: 42.5 MB (86.33%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,16 +1,14 @@
 class Solution {
-    private int solve(int ind,int[] nums,int[] dp){
-        if(ind<0) return 0;
-        if(dp[ind]!=-1) return dp[ind];
-        int left=nums[ind]+solve(ind-2,nums,dp);
-        int right=solve(ind-1,nums,dp);
-
-        return dp[ind]=Math.max(left,right);
-    }
     public int rob(int[] nums) {
         int n=nums.length;
+        if(n==1) return nums[0];
         int[] dp=new int[n];
-        for(int i=0;i<n;i++) dp[i]=-1;
-        return solve(n-1,nums,dp);
+        dp[0]=nums[0];dp[1]=Math.max(nums[0],nums[1]);
+        for(int i=2;i<n;i++){
+            int left=nums[i]+dp[i-2];
+            int right=dp[i-1];
+            dp[i]=Math.max(left,right);
+        }
+        return dp[n-1];
     }
 }
