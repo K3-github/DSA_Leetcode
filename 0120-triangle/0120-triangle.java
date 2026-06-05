ced5be06f2ce1dd9c54e8a37cb375fd8b0ae19a1
Krishnveer Bhagod <112793546+K3-github@users.noreply.github.com>
2026-06-05 12:35:07 +0530

    Time: 3 ms (83.84%), Space: 45.3 MB (83.16%) - LeetHub

--- 0120-triangle/0120-triangle.java
+++ 0120-triangle/0120-triangle.java
@@ -1,25 +1,20 @@
 class Solution {
     public int minimumTotal(List<List<Integer>> triangle) {
         int n=triangle.size();
-        int[] prev=new int[n];
+        int[] dp=new int[n];
         
-        Arrays.fill(prev,10000000);
-        
-        prev[0]=triangle.get(0).get(0);
+        dp[0]=triangle.get(0).get(0);
         for(int i=1;i<n;i++){
-            int[] curr=new int[n];
-            Arrays.fill(curr,10000000);
-            for(int j=0;j<triangle.get(i).size();j++){
-                  int left=10000000;
-                  if(j>0) left=prev[j-1];
-                  int right=prev[j];
-                  curr[j]=triangle.get(i).get(j)+Math.min(left,right);
+            for(int j=i;j>=0;j--){
+                  int left=10000000,right=10000000;
+                  if(j>0) left=dp[j-1];
+                  if(j<i) right=dp[j];
+                  dp[j]=triangle.get(i).get(j)+Math.min(left,right);
             }
-            prev=curr;
         }
         int ans=10000000;
         for(int i=0;i<n;i++){
-            ans=Math.min(ans,prev[i]);
+            ans=Math.min(ans,dp[i]);
         }
         return ans;
     }
