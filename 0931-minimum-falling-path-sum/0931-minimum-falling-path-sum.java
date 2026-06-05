8f98d770d5c3dfbbbc54d54f520cc41a5bb29bf9
Krishnveer Bhagod <112793546+K3-github@users.noreply.github.com>
2026-06-05 13:50:36 +0530

    Time: 6 ms (67.49%), Space: 47.5 MB (9.99%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.java
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.java
@@ -1,22 +1,24 @@
 class Solution {
     public int minFallingPathSum(int[][] mat) {
         int n=mat.length;
-        int[][] dp=new int[n][n];
+        int[] prev=new int[n];
         for(int j=0;j<n;j++){
-            dp[0][j]=mat[0][j];
+            prev[j]=mat[0][j];
         }
         for(int i=1;i<n;i++){
+            int[] curr=new int[n];
             for(int j=0;j<n;j++){
                 int left=10000000,right=10000000;
-                if(j>0) left=dp[i-1][j-1];
-                if(j<n-1) right=dp[i-1][j+1];
-                int up=dp[i-1][j];
-                dp[i][j]=mat[i][j]+Math.min(left,Math.min(up,right));
+                if(j>0) left=prev[j-1];
+                if(j<n-1) right=prev[j+1];
+                int up=prev[j];
+                curr[j]=mat[i][j]+Math.min(left,Math.min(up,right));
             }
+            prev=curr;
         }
         int ans=10000000;
         for(int j=0;j<n;j++){
-            ans=Math.min(ans,dp[n-1][j]);
+            ans=Math.min(ans,prev[j]);
         }
         return ans;
     }
