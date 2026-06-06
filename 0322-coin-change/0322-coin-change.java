982fc081676a0398f5f9aec650c5184266a407ad
Krishnveer Bhagod <112793546+K3-github@users.noreply.github.com>
2026-06-06 18:50:51 +0530

    Time: 15 ms (81.43%), Space: 46.7 MB (36.98%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,27 +1,28 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
         int n=coins.length;
-        int[][] dp=new int[n][amount+1];
-        for(int i=0;i<n;i++){
-            Arrays.fill(dp[i],10000000);
-        }
-        for(int i=0;i<n;i++) dp[i][0]=0;
+        int[] prev=new int[amount+1];
+        Arrays.fill(prev,10000000);
+        prev[0]=0;
         for(int amt=1;amt<=amount;amt++){
             if(coins[0]<=amt){
-                if(amt%coins[0]==0) dp[0][amt]=amt/coins[0];
+                if(amt%coins[0]==0) prev[amt]=amt/coins[0];
             }
         }
         for(int ind=1;ind<n;ind++){
+            int[] curr=new int[amount+1];
+            curr[0]=0;
             for(int amt=1;amt<=amount;amt++){
                 int w1=10000000;
                 if(coins[ind]<=amt){
-                    w1=1+dp[ind][amt-coins[ind]];
+                    w1=1+curr[amt-coins[ind]];
                 }
-                int w2=dp[ind-1][amt];
-                dp[ind][amt]=Math.min(w1,w2);
+                int w2=prev[amt];
+                curr[amt]=Math.min(w1,w2);
             }
+            prev=curr;
         }
-        int ans=dp[n-1][amount];
+        int ans=prev[amount];
         if(ans==10000000) return -1;
         return ans;
     }
