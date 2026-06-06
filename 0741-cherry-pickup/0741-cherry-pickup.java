b7484a181f9c8a35567f494e861f933bef94c160
Krishnveer Bhagod <112793546+K3-github@users.noreply.github.com>
2026-06-06 13:09:13 +0530

    Time: 15 ms (70.69%), Space: 47.1 MB (65.22%) - LeetHub

--- 0741-cherry-pickup/0741-cherry-pickup.java
+++ 0741-cherry-pickup/0741-cherry-pickup.java
@@ -1,34 +1,33 @@
 class Solution {
-    private int solve(int row1,int row2,int col1,int col2,int[][] grid,int[][][][] dp){
+    private int solve(int row1,int row2,int col1,int[][] grid,int[][][] dp){
+        int col2=row1+col1-row2;
         if(row1<0 || col1<0 || col2<0 || row2<0) return -100000000;
         if(grid[row1][col1]==-1 || grid[row2][col2]==-1) return -100000000;
         if(row1==0 && row2==0 && col1==0 && col2==0) return grid[row1][col1];
-        if(dp[row1][row2][col1][col2]!=-1) return dp[row1][row2][col1][col2];
+        if(dp[row1][row2][col1]!=-1) return dp[row1][row2][col1];
 
         int cherry=0;
         if(col1==col2) cherry=grid[row1][col1];
         else cherry=grid[row1][col1]+grid[row2][col2];
 
-        int w1=cherry+solve(row1,row2,col1-1,col2-1,grid,dp);
-        int w2=cherry+solve(row1-1,row2-1,col1,col2,grid,dp);
-        int w3=cherry+solve(row1,row2-1,col1-1,col2,grid,dp);
-        int w4=cherry+solve(row1-1,row2,col1,col2-1,grid,dp);
+        int w1=cherry+solve(row1,row2,col1-1,grid,dp);
+        int w2=cherry+solve(row1-1,row2-1,col1,grid,dp);
+        int w3=cherry+solve(row1,row2-1,col1-1,grid,dp);
+        int w4=cherry+solve(row1-1,row2,col1,grid,dp);
 
-        return dp[row1][row2][col1][col2]=Math.max(w1,Math.max(w2,Math.max(w3,w4)));
+        return dp[row1][row2][col1]=Math.max(w1,Math.max(w2,Math.max(w3,w4)));
     }
     public int cherryPickup(int[][] grid) {
         int n=grid.length;
-        int[][][][] dp=new int[n][n][n][n];
+        int[][][] dp=new int[n][n][n];
         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
                 for(int k=0;k<n;k++){
-                    for(int l=0;l<n;l++){
-                        dp[i][j][k][l]=-1;
-                    }
+                    dp[i][j][k]=-1;
                 }
             }
         }
-        int ans=solve(n-1,n-1,n-1,n-1,grid,dp);
+        int ans=solve(n-1,n-1,n-1,grid,dp);
         return ans<0 ? 0: ans;
     }
 }
