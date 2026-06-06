c405c5b3b9fcf7084bd4b175fd744f7ecf96b48b
Krishnveer Bhagod <112793546+K3-github@users.noreply.github.com>
2026-06-06 18:48:26 +0530

    Time: 20 ms (34.91%), Space: 47.2 MB (19.12%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,27 +1,28 @@
 class Solution {
-    private int solve(int ind,int amt,int[] coins,int[][] dp){
-        if(amt==0) return 0;
-        if(ind<0){
-            return 10000000;
-        }
-        if(dp[ind][amt]!=-1) return dp[ind][amt];
-
-        int w1=10000000;
-        if(coins[ind]<=amt){
-            w1=1+solve(ind,amt-coins[ind],coins,dp);
-        }
-        int w2=solve(ind-1,amt,coins,dp);
-        return dp[ind][amt]=Math.min(w1,w2);
-    }
     public int coinChange(int[] coins, int amount) {
         int n=coins.length;
         int[][] dp=new int[n][amount+1];
         for(int i=0;i<n;i++){
-            for(int j=0;j<=amount;j++){
-                dp[i][j]=-1;
+            Arrays.fill(dp[i],10000000);
+        }
+        for(int i=0;i<n;i++) dp[i][0]=0;
+        for(int amt=1;amt<=amount;amt++){
+            if(coins[0]<=amt){
+                if(amt%coins[0]==0) dp[0][amt]=amt/coins[0];
             }
         }
-        int ans=solve(n-1,amount,coins,dp);
-        return ans == 10000000 ? -1 : ans;
+        for(int ind=1;ind<n;ind++){
+            for(int amt=1;amt<=amount;amt++){
+                int w1=10000000;
+                if(coins[ind]<=amt){
+                    w1=1+dp[ind][amt-coins[ind]];
+                }
+                int w2=dp[ind-1][amt];
+                dp[ind][amt]=Math.min(w1,w2);
+            }
+        }
+        int ans=dp[n-1][amount];
+        if(ans==10000000) return -1;
+        return ans;
     }
 }
