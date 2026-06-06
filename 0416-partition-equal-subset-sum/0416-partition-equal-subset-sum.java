c41043936c4ad91e5f981da9e825bb1c879cc0a6
Krishnveer Bhagod <112793546+K3-github@users.noreply.github.com>
2026-06-06 14:56:35 +0530

    Time: 46 ms (57.84%), Space: 46.9 MB (64.5%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -5,19 +5,22 @@ class Solution {
         for(int i=0;i<n;i++) sum+=nums[i];
         if(sum%2!=0) return false;
         int req=sum/2;
-        boolean[][] dp=new boolean[n][req+1];
-        for(int i=0;i<n;i++) dp[i][0]=false;
+        boolean[] prev=new boolean[req+1];
+        for(int i=0;i<n;i++) prev[0]=false;
         if(nums[0]<=req){
-            dp[0][nums[0]]=true;
+            prev[nums[0]]=true;
         }
         for(int i=1;i<n;i++){
+            boolean[] cur=new boolean[req+1];
+            cur[0]=false;
             for(int curr=1;curr<=req;curr++){
                 boolean w1=false;
-                if(nums[i]<=curr) w1=dp[i-1][curr-nums[i]];
-                boolean w2=dp[i-1][curr];
-                dp[i][curr] = w1 || w2;
+                if(nums[i]<=curr) w1=prev[curr-nums[i]];
+                boolean w2=prev[curr];
+                cur[curr] = w1 || w2;
             }
+            prev=cur;
         }
-        return dp[n-1][req];
+        return prev[req];
     }
 }
