6a099803ad3ff29a3bd44cc92146c5283258df43
Krishnveer Bhagod <112793546+K3-github@users.noreply.github.com>
2026-06-07 13:40:50 +0530

    Time: 18 ms (93.86%), Space: 46.8 MB (91.62%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -3,22 +3,24 @@ class Solution {
         int n=t1.length();
         int m=t2.length();
         if(n==0 || m==0) return 0;
-        int[][] dp=new int[n+1][m+1];
+        int[] prev=new int[m+1];
 
         for(int i=1;i<=n;i++){
+            int[] curr=new int[m+1];
             for(int j=1;j<=m;j++){
                 char ch1=t1.charAt(i-1);
                 char ch2=t2.charAt(j-1);
                 if(ch1==ch2){
-                    dp[i][j]=1+dp[i-1][j-1];
+                    curr[j]=1+prev[j-1];
                 }
                 else{
-                    int w1=dp[i-1][j];
-                    int w2=dp[i][j-1];
-                    dp[i][j]=Math.max(w1,w2);
+                    int w1=prev[j];
+                    int w2=curr[j-1];
+                    curr[j]=Math.max(w1,w2);
                 } 
             }
+            prev=curr;
         }
-        return dp[n][m];
+        return prev[m];
     }
 }
