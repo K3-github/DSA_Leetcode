cca7090876a82165b202bd84752df82e4cec7736
Krishnveer Bhagod <112793546+K3-github@users.noreply.github.com>
2026-06-07 18:36:18 +0530

    Time: 41 ms (35.29%), Space: 47 MB (66.74%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.java
+++ 0044-wildcard-matching/0044-wildcard-matching.java
@@ -2,11 +2,8 @@ class Solution {
     public boolean isMatch(String s, String p) {
         int n=s.length();
         int m=p.length();
-        boolean[][] dp=new boolean[n+1][m+1];
-        dp[0][0]=true;
-        for(int i=1;i<=n;i++){
-            dp[i][0]=false;
-        }
+        boolean[] prev=new boolean[m+1];
+        prev[0]=true;
         for(int j=1;j<=m;j++){
             boolean flag=true;
             for(int ptr=j;ptr>=1;ptr--){
@@ -15,23 +12,26 @@ class Solution {
                     break;
                 }
             }
-            dp[0][j]=(flag==true ? true : false);
+            prev[j]=(flag==true ? true : false);
         }
         for(int i=1;i<=n;i++){
+            boolean[] curr=new boolean[m+1];
+            curr[0]=false;
             for(int j=1;j<=m;j++){
                 char ch1=s.charAt(i-1);
                 char ch2=p.charAt(j-1);
                 if(ch1==ch2 || ch2=='?'){
-                    dp[i][j]=dp[i-1][j-1];
+                    curr[j]=prev[j-1];
                 }
                 else if(ch2=='*'){
-                    boolean w1=dp[i-1][j];
-                    boolean w2=dp[i][j-1];
-                    dp[i][j]=w1 || w2;
+                    boolean w1=prev[j];
+                    boolean w2=curr[j-1];
+                    curr[j]=w1 || w2;
                 }
-                else dp[i][j]=false;
+                else curr[j]=false;
             }
+            prev=curr;
         }
-        return dp[n][m];
+        return prev[m];
     }
 }
