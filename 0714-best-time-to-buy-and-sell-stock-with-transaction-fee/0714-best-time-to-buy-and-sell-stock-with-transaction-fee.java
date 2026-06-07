b63368608c2b8584553cabba3bbd4f170e8c8e34
Krishnveer Bhagod <112793546+K3-github@users.noreply.github.com>
2026-06-07 20:55:56 +0530

    Time: 30 ms (27.84%), Space: 77.4 MB (26.24%) - LeetHub

---
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.java
@@ -0,0 +1,30 @@
+class Solution {
+    int fees;
+    private int solve(int ind,int buy,int[] p,int[][] dp){
+        int n=p.length;
+        if(ind>=n) return 0;
+        if(dp[ind][buy]!=-1) return dp[ind][buy];
+
+        if(buy==1){
+            int w1=(p[ind]-fees)+solve(ind+1,0,p,dp);
+            int w2=solve(ind+1,1,p,dp);
+            return dp[ind][buy]=Math.max(w1,w2);
+        }
+        else{
+            int w1=-p[ind]+solve(ind+1,1,p,dp);
+            int w2=solve(ind+1,0,p,dp);
+            return dp[ind][buy]=Math.max(w1,w2);
+        }
+    }
+    public int maxProfit(int[] prices,int fee) {
+        int n=prices.length;
+        int[][] dp=new int[n][2];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<2;j++){
+                dp[i][j]=-1;
+            }
+        }
+        fees=fee;
+        return solve(0,0,prices,dp);
+    }
+}
