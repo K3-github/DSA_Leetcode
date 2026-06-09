cf740afb814276b6754df2d0b444f0c124acc692
Krishnveer Bhagod <112793546+K3-github@users.noreply.github.com>
2026-06-10 00:45:09 +0530

    Time: 5 ms (88.55%), Space: 46.2 MB (54.59%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,17 +1,34 @@
 class Solution {
-    public int lengthOfLIS(int[] nums) {
-        int n=nums.length;
-        int[] dp=new int[n];
-        for(int i=0;i<n;i++) dp[i]=1;
-        int ans=1;
-        for(int ind=0;ind<n;ind++){
-            for(int prev=ind-1;prev>=0;prev--){
-                 if(nums[ind]>nums[prev]){
-                    dp[ind]=Math.max(dp[ind],1+dp[prev]);
-                 }
+    private int findLowerBound(List<Integer> arr,int x){
+        int n=arr.size();
+        if(arr.get(0)>=x) return 0;
+        if(arr.get(n-1)<x) return n;
+        int low=0,high=n-1;
+        int ans=-1;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(arr.get(mid)>=x){
+                ans=mid;
+                high=mid-1;
             }
-            ans=Math.max(ans,dp[ind]);
+            else low=mid+1;
         }
         return ans;
+    }
+    public int lengthOfLIS(int[] nums) {
+        List<Integer> vec=new ArrayList<>();
+        int n=nums.length;
+
+        for(int i=0;i<n;i++){
+            if(vec.size()==0){
+                vec.add(nums[i]);
+            }
+            else{
+                int ind=findLowerBound(vec,nums[i]);
+                if(ind==vec.size()) vec.add(nums[i]);
+                else vec.set(ind,nums[i]);
+            }
+        }
+        return vec.size();
     }
 }
