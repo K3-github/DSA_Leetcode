fc95a4f40515988884191a39dd5d23934b2efab8
Krishnveer Bhagod <112793546+K3-github@users.noreply.github.com>
2026-06-10 16:00:35 +0530

    Time: 3 ms (72.55%), Space: 47 MB (87.49%) - LeetHub

--- 0845-longest-mountain-in-array/0845-longest-mountain-in-array.java
+++ 0845-longest-mountain-in-array/0845-longest-mountain-in-array.java
@@ -1,20 +1,37 @@
 class Solution {
+    private void rev(int[] right){
+        int i=0,j=right.length-1;
+        while(i<j){
+            int tp=right[i];
+            right[i]=right[j];
+            right[j]=tp;
+            i++;j--;
+        }
+        return;
+    }
     public int longestMountain(int[] arr) {
         int n=arr.length;
-        if(n<=2) return 0;
-        int ans=0;
-        for(int i=1;i<n-1;i++){
-            if(arr[i]>arr[i-1] && arr[i]>arr[i+1]){
-                int j=i;
-                while(j>0 && arr[j-1]<arr[j]){
-                    j--;
-                }
-                int k=i;
-                while(k<n-1 && arr[k+1]<arr[k]){
-                    k++;
-                }
-                ans=Math.max(ans,k-j+1);
+        int[] left=new int[n];
+        int[] right=new int[n];
+
+        for(int i=0;i<n;i++){
+            left[i]=1;
+            right[i]=1;
+        }
+        for(int i=1;i<n;i++){
+            if(arr[i]>arr[i-1]){
+                left[i]+=left[i-1];
             }
+        }
+        for(int i=n-2;i>=0;i--){
+            if(arr[i]>arr[i+1]){
+                right[i]+=right[i+1];
+            }
+        }
+        int ans=0;
+        for(int i=0;i<n;i++){
+            if(left[i]==1 || right[i]==1) continue;
+            ans=Math.max(ans,left[i]+right[i]-1);
         }
         return ans;
     }
