8d2de832fcdf43ff0175860ad02bac3446ee82e5
Krishnveer Bhagod <112793546+K3-github@users.noreply.github.com>
2026-06-12 17:50:19 +0530

    Time: 1 ms (100%), Space: 43.1 MB (97.47%) - LeetHub

---
+++ 2767-partition-string-into-minimum-beautiful-substrings/2767-partition-string-into-minimum-beautiful-substrings.java
@@ -0,0 +1,27 @@
+class Solution {
+    private boolean isPowerOf5(String s, int i, int j) {
+        int n=0;
+        for(int k=i;k<=j;k++) {
+            n=(n<<1)+(s.charAt(k)-'0');
+        }
+        return n>0 && 1220703125%n==0;
+      }  
+    private int solve(int ind,int n,String s){
+        if(ind>=n) return 0;
+        int ans=Integer.MAX_VALUE;
+        for(int j=ind;j<n;j++){
+            if(s.charAt(ind)=='0') continue;
+            if(isPowerOf5(s,ind,j)==true){
+                int par=solve(j+1,n,s);
+                if(par!=Integer.MAX_VALUE) ans=Math.min(ans,1+par);
+            }
+        }
+        return ans;
+    }
+    public int minimumBeautifulSubstrings(String s) {
+        int n=s.length();
+        if(s.charAt(0)=='0') return -1;
+        int ans=solve(0,n,s);
+        return ans==Integer.MAX_VALUE ? -1 : ans;
+    }
+}
