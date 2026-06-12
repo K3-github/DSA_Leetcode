cbbe3c3770001f23dd3776d1bba1d81dd31776ea
Krishnveer Bhagod <112793546+K3-github@users.noreply.github.com>
2026-06-12 17:51:50 +0530

    Time: 1 ms (100%), Space: 43.1 MB (97.47%) - LeetHub

--- 2767-partition-string-into-minimum-beautiful-substrings/2767-partition-string-into-minimum-beautiful-substrings.java
+++ 2767-partition-string-into-minimum-beautiful-substrings/2767-partition-string-into-minimum-beautiful-substrings.java
@@ -6,22 +6,25 @@ class Solution {
         }
         return n>0 && 1220703125%n==0;
       }  
-    private int solve(int ind,int n,String s){
+    private int solve(int ind,int n,String s,int[] dp){
         if(ind>=n) return 0;
+        if(dp[ind]!=-1) return dp[ind];
         int ans=Integer.MAX_VALUE;
         for(int j=ind;j<n;j++){
             if(s.charAt(ind)=='0') continue;
             if(isPowerOf5(s,ind,j)==true){
-                int par=solve(j+1,n,s);
+                int par=solve(j+1,n,s,dp);
                 if(par!=Integer.MAX_VALUE) ans=Math.min(ans,1+par);
             }
         }
-        return ans;
+        return dp[ind]=ans;
     }
     public int minimumBeautifulSubstrings(String s) {
         int n=s.length();
         if(s.charAt(0)=='0') return -1;
-        int ans=solve(0,n,s);
+        int[] dp=new int[n];
+        for(int i=0;i<n;i++) dp[i]=-1;
+        int ans=solve(0,n,s,dp);
         return ans==Integer.MAX_VALUE ? -1 : ans;
     }
 }
