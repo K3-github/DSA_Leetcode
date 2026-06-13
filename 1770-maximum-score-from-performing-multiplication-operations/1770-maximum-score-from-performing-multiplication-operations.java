2abf487a6334569a8a4f9ae1f3ff27770d7a57f5
Krishnveer Bhagod <112793546+K3-github@users.noreply.github.com>
2026-06-14 01:43:54 +0530

    Time: 13 ms (52%), Space: 64 MB (73.78%) - LeetHub

--- 1770-maximum-score-from-performing-multiplication-operations/1770-maximum-score-from-performing-multiplication-operations.java
+++ 1770-maximum-score-from-performing-multiplication-operations/1770-maximum-score-from-performing-multiplication-operations.java
@@ -1,24 +1,22 @@
 class Solution {
-    private int solve(int i,int j,int ind,int[] multipliers,int[] nums,int[][][] dp){
+    private int solve(int i,int j,int ind,int[] multipliers,int[] nums,int[][] dp){
         int m=multipliers.length;
         int n=nums.length;
         if(ind>=m) return 0;
-        if(dp[i][j-n+m][ind]!=-1) return dp[i][j-n+m][ind];
+        if(dp[i][j-n+m]!=-1) return dp[i][j-n+m];
 
         int w1=multipliers[ind]*nums[i]+solve(i+1,j,ind+1,multipliers,nums,dp);
         int w2=multipliers[ind]*nums[j]+solve(i,j-1,ind+1,multipliers,nums,dp);
 
-        return dp[i][j-n+m][ind]=Math.max(w1,w2);
+        return dp[i][j-n+m]=Math.max(w1,w2);
     }
     public int maximumScore(int[] nums, int[] multipliers) {
         int n=nums.length;
         int m=multipliers.length;
-        int[][][] dp=new int[m][m][m];
+        int[][] dp=new int[m][m];
         for(int i=0;i<m;i++){
             for(int j=0;j<m;j++){
-                for(int k=0;k<m;k++){
-                    dp[i][j][k]=-1;
-                }
+                 dp[i][j]=-1;
             }
         }
         return solve(0,n-1,0,multipliers,nums,dp);
