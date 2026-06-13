e39c265e360e2cd1ef34b60ed825814cdd78e6c8
Krishnveer Bhagod <112793546+K3-github@users.noreply.github.com>
2026-06-13 17:05:37 +0530

    Time: 6 ms (24.19%), Space: 46.8 MB (63.37%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
@@ -8,36 +8,29 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
-class Solution {
-    private ListNode mergeTwoSortedList(ListNode head1,ListNode head2){
-        if(head1==null) return head2;
-        if(head2==null) return head1;
-        ListNode l1=head1,l2=head2;
-        if(head1.val>head2.val){
-            l1=head2;l2=head1;
-        }
-        while(l1!=null){
-            ListNode prev=l1;
-            while(l1!=null && l1.val<=l2.val){
-                prev=l1; l1=l1.next;
-            }
-            prev.next=l2;
-            if(l1!=null){
-                ListNode t=l1;
-                l1=l2;
-                l2=t;
-            }
-        }
-        return head1.val<=head2.val ? head1 : head2;
-    }
-    private ListNode solve(int ind,ListNode[] lists){
-        if(ind<0) return null;
+class Pair{
+    int first;
+    ListNode second;
 
-        ListNode newListNode=solve(ind-1,lists);
-        return mergeTwoSortedList(lists[ind],newListNode);
+    Pair(int first,ListNode second){
+        this.first=first;
+        this.second=second;
     }
+}; 
+class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
-        int n=lists.length;
-        return solve(n-1,lists);
+        PriorityQueue<Pair> pq=new PriorityQueue<>((a,b) -> a.first - b.first);
+        for(ListNode node : lists){
+            if(node!=null) pq.offer(new Pair(node.val,node));
+        }
+        ListNode d=new ListNode(-1);
+        ListNode curr=d;
+        while(!pq.isEmpty()){
+            Pair tp=pq.poll();
+            curr.next=tp.second;
+            curr=curr.next;
+            if(tp.second.next!=null) pq.offer(new Pair(tp.second.next.val,tp.second.next));
+        }
+        return d.next;
     }
 }
