06f8dee6572472021e00c605fd19aaa22199829e
Krishnveer Bhagod <112793546+K3-github@users.noreply.github.com>
2026-06-14 13:48:53 +0530

    Time: 7 ms (89.05%), Space: 46.1 MB (64.48%) - LeetHub

---
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int characterReplacement(String s, int k) {
+        int n=s.length();
+        int[] freq=new int[26];
+        int i=0,j=0;
+        int maxF=0;
+        int ans=0;
+        while(j<n){
+             char ch=s.charAt(j);
+             freq[ch-'A']++;
+             maxF=Math.max(maxF,freq[ch-'A']);
+             
+             while(j-i+1-maxF>k){
+                char ch1=s.charAt(i);
+                freq[ch1-'A']--;
+                maxF=0;
+                for(int ptr=0;ptr<26;ptr++) maxF=Math.max(maxF,freq[ptr]);
+                i++;
+             }
+             ans=Math.max(ans,j-i+1);
+             j++;
+        }
+        return ans;
+    }
+}
