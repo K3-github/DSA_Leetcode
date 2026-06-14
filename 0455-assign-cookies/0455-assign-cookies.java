76f3139524f21ea286f8f5faaa87337987b9d8bf
Krishnveer Bhagod <112793546+K3-github@users.noreply.github.com>
2026-06-14 19:08:01 +0530

    Time: 15 ms (63.1%), Space: 51.4 MB (73.2%) - LeetHub

---
+++ 0455-assign-cookies/0455-assign-cookies.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int i=0,j=0;
+        int n=g.length,m=s.length;
+        while(i<n && j<m){
+              if(g[i]<=s[j]) i++;
+              j++;
+        }
+        return i;
+    }
+}
