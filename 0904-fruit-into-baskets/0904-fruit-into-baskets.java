c4f37954ead61a1dff806403411b243693c9e0ad
Krishnveer Bhagod <112793546+K3-github@users.noreply.github.com>
2026-06-14 11:35:52 +0530

    Time: 53 ms (81.68%), Space: 68.5 MB (88.07%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.java
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.java
@@ -6,12 +6,12 @@ class Solution {
         int n=fruits.length;
         while(j<n){
             mp.put(fruits[j],mp.getOrDefault(fruits[j],0)+1);
-            while(mp.size()>2){
+            if(mp.size()>2){
                 mp.put(fruits[i],mp.get(fruits[i])-1);
                 if(mp.get(fruits[i])==0) mp.remove(fruits[i]);
                 i++;
             }
-            ans=Math.max(ans,j-i+1);
+            else ans=Math.max(ans,j-i+1);
             j++;
         }
         return ans;
