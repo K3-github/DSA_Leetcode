a82fac82331735847008d00c65403bb246ace06e
Krishnveer Bhagod <112793546+K3-github@users.noreply.github.com>
2026-06-14 14:27:31 +0530

    Time: 2 ms (60.52%), Space: 51 MB (51.69%) - LeetHub

--- 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.java
+++ 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.java
@@ -12,21 +12,21 @@ class Solution {
        }
        return count;
     }
-    private int countSubarraySumGreaterThanGoal(int[] nums,int goal){
-       int n=nums.length;
-       int i=0,j=0,count=0,sum=0;
-       while(j<n){
-           sum+=nums[j];
-           while(i<=j && sum>goal){
-              count+=n-j;
-              sum-=nums[i++];
-           }
-           j++;
-       }
-       return count;
-    }
+    // private int countSubarraySumGreaterThanGoal(int[] nums,int goal){
+    //    int n=nums.length;
+    //    int i=0,j=0,count=0,sum=0;
+    //    while(j<n){
+    //        sum+=nums[j];
+    //        while(i<=j && sum>goal){
+    //           count+=n-j;
+    //           sum-=nums[i++];
+    //        }
+    //        j++;
+    //    }
+    //    return count;
+    // }
     public int numSubarraysWithSum(int[] nums, int goal) {
         return countSubarraySumGreaterThanOrEqualGoal(nums,goal)
-                -countSubarraySumGreaterThanGoal(nums,goal);
+                -countSubarraySumGreaterThanOrEqualGoal(nums,goal+1);
     }
 }
