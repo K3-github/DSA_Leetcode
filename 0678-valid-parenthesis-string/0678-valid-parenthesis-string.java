ac27c24d90518dc2f195cddce4f35c28901288c4
Krishnveer Bhagod <112793546+K3-github@users.noreply.github.com>
2026-06-18 00:03:43 +0530

    Time: 0 ms (100%), Space: 42.6 MB (65.78%) - LeetHub

--- 0678-valid-parenthesis-string/0678-valid-parenthesis-string.java
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.java
@@ -1,39 +1,36 @@
 class Solution {
-    private int solve(int ind,int cnt,String s,int[][] dp){
-        int n=s.length();
-        if(ind>=n){
-            if(cnt==0) return 1;
-            return 0;
-        }
-        if(dp[ind][cnt]!=-1) return dp[ind][cnt];
-
-        char ch=s.charAt(ind);
-        if(ch=='('){
-            return dp[ind][cnt]=solve(ind+1,cnt+1,s,dp);
-        }
-        else if(ch==')'){
-            if(cnt==0) return 0;
-            return dp[ind][cnt]=solve(ind+1,cnt-1,s,dp);
-        }
-        else{
-            int w1=solve(ind+1,cnt+1,s,dp);
-            int w2=0;
-            if(cnt>0){
-                w2=solve(ind+1,cnt-1,s,dp);
-            }
-            int w3=solve(ind+1,cnt,s,dp);
-            return dp[ind][cnt]=(w1==1 || w2==1 || w3==1) ? 1 : 0;
-        }
-    }
     public boolean checkValidString(String s) {
+        int cnt=0;
+        int stars=0;
         int n=s.length();
-        int[][] dp=new int[n][n+1];
         for(int i=0;i<n;i++){
-            for(int j=0;j<n+1;j++){
-                dp[i][j]=-1;
+            char ch=s.charAt(i);
+            if(ch=='(') cnt++;
+            else if(ch==')'){
+                if(cnt>0) cnt--;
+                else{
+                    if(stars==0) return false;
+                    stars--;
+                }
             }
+            else stars++;
         }
-        int ans=solve(0,0,s,dp);
-        return ans==0 ? false : true;
+        if(cnt>stars) return false;
+        cnt=0;stars=0;
+        for(int i=n-1;i>=0;i--){
+            char ch=s.charAt(i);
+            if(ch==')') cnt++;
+            else if(ch=='('){
+                if(cnt>0) cnt--;
+                else{
+                    if(stars==0) return false;
+                    stars--;
+                }
+            }
+            else stars++;
+        }
+        if(cnt>stars) return false;
+        return true;
     }
 }
+// ((((****
