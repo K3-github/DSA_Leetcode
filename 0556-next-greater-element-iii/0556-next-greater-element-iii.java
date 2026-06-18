0805edb613bb16538ba25b8e794bc2b507ee7dd8
Krishnveer Bhagod <112793546+K3-github@users.noreply.github.com>
2026-06-18 11:04:53 +0530

    Time: 0 ms (100%), Space: 42.2 MB (40.76%) - LeetHub

--- 0556-next-greater-element-iii/0556-next-greater-element-iii.java
+++ 0556-next-greater-element-iii/0556-next-greater-element-iii.java
@@ -1,33 +1,31 @@
 class Solution {
     public int nextGreaterElement(int n) {
-        if(n>=Integer.MAX_VALUE) return -1;
-        int k=-1;
-        List<Integer> arr=new ArrayList<>();
-        while(n>0){
-            arr.add(n%10);
-            n/=10;
-        }
-        int sz=arr.size();
-        for(int i=0;i<sz-1;i++){
-            if(arr.get(i)>arr.get(i+1)){
-                k=i+1;
+        char[] arr=String.valueOf(n).toCharArray();
+        int pivot=-1;
+        int len=arr.length;
+        for(int i=len-2;i>=0;i--){
+            if(arr[i]<arr[i+1]){
+                pivot=i;
                 break;
             }
         }
-        if(k==-1) return -1;
-        for(int i=0;i<sz;i++){
-            if(arr.get(i)>arr.get(k)){
-                int tp=arr.get(i);
-                arr.set(i,arr.get(k));
-                arr.set(k,tp);
-                Collections.reverse(arr.subList(0,k));
+        if(pivot==-1) return -1;
+        for(int i=len-1;i>=0;i--){
+            if(arr[i]>arr[pivot]){
+                char tp=arr[i];
+                arr[i]=arr[pivot];
+                arr[pivot]=tp;
                 break;
             }
         }
-        long ans=0;
-        for(int i=sz-1;i>=0;i--){
-            ans=ans*10+arr.get(i);
+        int l=pivot+1,r=len-1;
+        while(l<r){
+            char tp=arr[l];
+            arr[l]=arr[r];
+            arr[r]=tp;
+            l++;r--;
         }
+        long ans=Long.parseLong(new String(arr));
         if(ans>Integer.MAX_VALUE) return -1;
         return (int)ans;
     }
