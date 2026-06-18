3eef23cab9b16895d2e041e84550c5f26642891d
Krishnveer Bhagod <112793546+K3-github@users.noreply.github.com>
2026-06-18 17:24:29 +0530

    Time: 19 ms (64.3%), Space: 47.8 MB (6.52%) - LeetHub

--- 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.java
+++ 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.java
@@ -1,15 +1,52 @@
 class Solution {
     public long subArrayRanges(int[] nums) {
         int n=nums.length;
+        int[] nextG=new int[n];
+        int[] nextS=new int[n];
+        int[] prevG=new int[n];
+        int[] prevS=new int[n];
+        for(int i=0;i<n;i++){
+            prevG[i]=-1;prevS[i]=-1;
+            nextG[i]=n;nextS[i]=n;
+        }
+
+        Stack<Integer> st=new Stack<>();
+        for(int i=0;i<n;i++){
+            while(st.size()>0 && nums[st.peek()]<nums[i]){
+                st.pop();
+            }
+            if(st.size()>0) prevG[i]=st.peek();
+            st.push(i);
+        }
+        st.clear();
+        for(int i=n-1;i>=0;i--){
+            while(st.size()>0 && nums[st.peek()]<=nums[i]){
+                st.pop();
+            }
+            if(st.size()>0) nextG[i]=st.peek();
+            st.push(i);
+        }
+        st.clear();
+        for(int i=0;i<n;i++){
+            while(st.size()>0 && nums[st.peek()]>nums[i]){
+                st.pop();
+            }
+            if(st.size()>0) prevS[i]=st.peek();
+            st.push(i);
+        }
+        st.clear();
+        for(int i=n-1;i>=0;i--){
+            while(st.size()>0 && nums[st.peek()]>=nums[i]){
+                st.pop();
+            }
+            if(st.size()>0) nextS[i]=st.peek();
+            st.push(i);
+        }
         long ans=0;
         for(int i=0;i<n;i++){
-            int mn=nums[i],mx=nums[i];
-            ans+=mx-mn;
-            for(int j=i+1;j<n;j++){
-                 mn=Math.min(mn,nums[j]);
-                 mx=Math.max(mx,nums[j]);
-                 ans+=mx-mn;
-            }
+            int noOfTimesMin=(nextS[i]-i)*(i-prevS[i]);
+            int noOfTimesMax=(nextG[i]-i)*(i-prevG[i]);
+            ans+=(noOfTimesMax-noOfTimesMin)*(long)nums[i];
         }
         return ans;
     }
