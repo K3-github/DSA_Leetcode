11e985b2feb0770163249336e32cbfdf892f0409
Krishnveer Bhagod <112793546+K3-github@users.noreply.github.com>
2026-06-21 00:01:56 +0530

    Time: 0 ms (100%), Space: 41.9 MB (75.55%) - LeetHub

--- 0397-integer-replacement/0397-integer-replacement.java
+++ 0397-integer-replacement/0397-integer-replacement.java
@@ -1,17 +1,19 @@
 class Solution {
-    private int solve(long num){
-        if(num==1) return 0;
-        if(num<1) return 1000000;
-
-        if(num%2==0){
-            return 1+solve(num/2);
-        }
-        else{
-            return 1+Math.min(solve(num-1),solve(num+1));
-        }
-    }
     public int integerReplacement(int n) {
         long num=n;
-        return solve(num);
+        int count=0;
+
+        while(num!=1){
+            if((num&1)==0){
+                num>>=1;
+            }
+            else{
+                if(num==3) num--;
+                else if(((num>>1)&1)==1) num++;
+                else num--;
+            }
+            count++;
+        }
+        return count;
     }
 }
