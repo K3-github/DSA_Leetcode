5e619ba0ac954976e8e2b3cba2fb5d8de3f9fe3c
Krishnveer Bhagod <112793546+K3-github@users.noreply.github.com>
2026-06-22 00:55:54 +0530

    Time: 381 ms (18.38%), Space: 42.8 MB (52.45%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.java
+++ 0037-sudoku-solver/0037-sudoku-solver.java
@@ -1,27 +1,23 @@
 class Solution {
-    HashMap<Integer,HashSet<Character>> row=new HashMap<>();
-    HashMap<Integer,HashSet<Character>> col=new HashMap<>();
-    HashMap<Integer,HashSet<Character>> box=new HashMap<>();
+    private boolean isValid(int row,int col,char[][] board,char ch){
+        for(int i=0;i<9;i++){
+            if(board[i][col]==ch) return false;
+            if(board[row][i]==ch) return false;
+            if(board[3*(row/3)+(i/3)][3*(col/3)+(i%3)]==ch) return false;
+        }
+        return true;
+    }
     private boolean solve(char[][] board){
         int n=board.length;
         int m=board[0].length;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(board[i][j]=='.'){
-                    for(char d='1';d<='9';d++){
-                        int boxId=3*(i/3)+(j/3); 
-                        if(row.get(i).contains(d) || col.get(j).contains(d) ||box.get(boxId).contains(d)) continue;
-
-                        board[i][j]=d;
-                        row.get(i).add(board[i][j]);
-                        col.get(j).add(board[i][j]);
-                        box.get(boxId).add(board[i][j]);
-                        if(solve(board)==true) return true;
-                        else{
-                            board[i][j]='.';
-                            row.get(i).remove(d);
-                            col.get(j).remove(d);
-                            box.get(boxId).remove(d);
+                    for(char ch='1';ch<='9';ch++){
+                        if(isValid(i,j,board,ch)){
+                            board[i][j]=ch;
+                            if(solve(board)==true) return true;
+                            else board[i][j]='.';
                         }
                     }
                     return false;
@@ -31,24 +27,6 @@ class Solution {
         return true;
     }
     public void solveSudoku(char[][] board) {
-        int n=board.length;
-        int m=board[0].length;
-
-        for(int i=0;i<9;i++){
-            row.put(i,new HashSet<>());
-            col.put(i,new HashSet<>());
-            box.put(i,new HashSet<>());
-        }
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(board[i][j]!='.'){
-                    row.get(i).add(board[i][j]);
-                    col.get(j).add(board[i][j]);
-                    int boxId=3*(i/3)+(j/3);
-                    box.get(boxId).add(board[i][j]);
-                }
-            }
-        }
         solve(board);
     }
 }
