53ba02d6ececec64948f89039ec5afe539748fe2
Krishnveer Bhagod <112793546+K3-github@users.noreply.github.com>
2026-06-23 02:16:21 +0530

    Time: 129 ms (48.88%), Space: 52.7 MB (70.46%) - LeetHub

--- 0282-expression-add-operators/0282-expression-add-operators.java
+++ 0282-expression-add-operators/0282-expression-add-operators.java
@@ -1,47 +1,5 @@
 class Solution {
     List<String> ans=new ArrayList<>();
-    private boolean isValid(String curr,int target){
-        Stack<String> st=new Stack<>();
-        int n=curr.length();
-        for(int i=0;i<n;i++){
-            StringBuilder num=new StringBuilder();
-            while(i<n && Character.isDigit(curr.charAt(i))){
-                num.append(curr.charAt(i));
-                i++;
-            }
-            if(i<n && curr.charAt(i)=='*'){
-                long val=Long.parseLong(num.toString());
-                while(i<n && curr.charAt(i)=='*'){
-                    i++;
-                    StringBuilder next=new StringBuilder();
-                    while(i<n && Character.isDigit(curr.charAt(i))){
-                        next.append(curr.charAt(i));
-                        i++;
-                    }
-                    val*=Long.parseLong(next.toString());
-                }
-                st.push(String.valueOf(val));
-            }
-            else{
-                st.push(num.toString());
-            }
-            if(i<n){
-                st.push(String.valueOf(curr.charAt(i)));
-            }
-        }
-        long res=Long.parseLong(st.pop());
-        while(!st.isEmpty()){
-            String sign=st.pop();
-            long num=Long.parseLong(st.pop());
-            if(sign.equals("+")){
-                res=num+res;
-            }
-            else if(sign.equals("-")){
-                res=num-res;
-            }
-        }
-        return res==target;
-    }
     private void solve(int ind,String curr,long value,long prev,int target,String num){
         if(ind==num.length()){
             if(value==target){
