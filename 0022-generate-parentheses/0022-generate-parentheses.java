5cb2f237321b7d0e6327c2bff614bef78a3f88b0
Krishnveer Bhagod <112793546+K3-github@users.noreply.github.com>
2026-06-22 15:46:46 +0530

    Time: 2 ms (71.99%), Space: 44.6 MB (65.92%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -0,0 +1,17 @@
+class Solution {
+    List<String> ans=new ArrayList<>();
+    private void solve(int open,int close,String str){
+        if(open==0 && close==0){
+            ans.add(str);
+            return;
+        }
+        if(open>0) solve(open-1,close,str+'(');
+        if(open==0 || open<close){
+            solve(open,close-1,str+')');
+        }
+    }
+    public List<String> generateParenthesis(int n) {
+        solve(n,n,"");
+        return ans;
+    }
+}
