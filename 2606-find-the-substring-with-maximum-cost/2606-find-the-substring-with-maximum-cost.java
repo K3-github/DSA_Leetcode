1fd336d7928dc62ab50f0d1353a003418346c9b2
Krishnveer Bhagod <112793546+K3-github@users.noreply.github.com>
2026-06-25 03:20:45 +0530

    Time: 3 ms (100%), Space: 47.1 MB (73.49%) - LeetHub

---
+++ 2606-find-the-substring-with-maximum-cost/2606-find-the-substring-with-maximum-cost.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maximumCostSubstring(String s, String chars, int[] vals) {
+        int[] cost=new int[26];
+        for(int i=0;i<26;i++) cost[i]=i+1;
+        for(int i=0;i<chars.length();i++) cost[chars.charAt(i)-'a']=vals[i];
+
+        int curr=0;
+        int ans=0;
+        for(char ch:s.toCharArray()){
+            int val=cost[ch-'a'];
+            curr=Math.max(val, curr+val);
+            ans=Math.max(ans,curr);
+        }
+        return ans;
+    }
+}
