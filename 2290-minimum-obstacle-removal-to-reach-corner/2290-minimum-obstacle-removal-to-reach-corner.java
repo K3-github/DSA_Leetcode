ac43ef587ee5986b4109fb50a56cb4dae9232125
Krishnveer Bhagod <112793546+K3-github@users.noreply.github.com>
2026-06-26 03:09:17 +0530

    Time: 163 ms (13.57%), Space: 116.4 MB (97.29%) - LeetHub

--- 2290-minimum-obstacle-removal-to-reach-corner/2290-minimum-obstacle-removal-to-reach-corner.java
+++ 2290-minimum-obstacle-removal-to-reach-corner/2290-minimum-obstacle-removal-to-reach-corner.java
@@ -10,29 +10,6 @@ class Pair{
     }
 };
 class Solution {
-    private int solve(int row,int col,int[][] grid,int[][] vis){
-        int n=grid.length;
-        int m=grid[0].length;
-        if(row<0 || row>=n || col<0 || col>=m || vis[row][col]==1) return 1000000;
-        if(row==0 && col==0) return 0;
-
-        vis[row][col]=1;
-        int[] drow=new int[]{0,+1,0,-1};
-        int[] dcol=new int[]{+1,0,-1,0};
-        int ans=1000000;
-        for(int d=0;d<4;d++){
-            int nrow=row+drow[d];
-            int ncol=col+dcol[d];
-            if(grid[row][col]==1){
-              ans=Math.min(ans,1+solve(nrow,ncol,grid,vis));
-            }
-            else{
-              ans=Math.min(ans,0+solve(nrow,ncol,grid,vis));
-            }
-        }
-        vis[row][col]=0;
-        return ans;
-    }
     public int minimumObstacles(int[][] grid) {
          int n=grid.length;
          int m=grid[0].length;
