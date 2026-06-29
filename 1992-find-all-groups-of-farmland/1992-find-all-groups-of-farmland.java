37b89bcb40a4f7ed078e7f7aff47f5ef202d5eec
Krishnveer Bhagod <112793546+K3-github@users.noreply.github.com>
2026-06-30 00:40:59 +0530

    Time: 19 ms (24.6%), Space: 121.2 MB (6.45%) - LeetHub

---
+++ 1992-find-all-groups-of-farmland/1992-find-all-groups-of-farmland.java
@@ -0,0 +1,40 @@
+class Solution {
+    int mxRow,mxCol;
+    private void solve(int row,int col,int[][] land){
+        int n=land.length;
+        int m=land[0].length;
+        if(row<0 || row>=n || col<0 || col>=m || land[row][col]==0) return;
+
+        land[row][col]=0;
+        mxRow=Math.max(mxRow,row);
+        mxCol=Math.max(mxCol,col);
+        int[] drow=new int[]{0,+1,0,-1};
+        int[] dcol=new int[]{+1,0,-1,0};
+
+        for(int d=0;d<4;d++){
+            int nrow=row+drow[d];
+            int ncol=col+dcol[d];
+            solve(nrow,ncol,land);
+        }
+    }
+    public int[][] findFarmland(int[][] land) {
+        List<int[]> ans=new ArrayList<>();
+        int n=land.length;
+        int m=land[0].length;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                mxRow=0;mxCol=0;
+                if(land[i][j]==1){
+                    solve(i,j,land);
+                    ans.add(new int[]{i,j,mxRow,mxCol});
+                }
+            }
+        }
+        int size=ans.size();
+        int[][] res=new int[size][4];
+        for(int i=0;i<size;i++){
+            res[i]=ans.get(i);
+        }
+        return res;
+    }
+}
