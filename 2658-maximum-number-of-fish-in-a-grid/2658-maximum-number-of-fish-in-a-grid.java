1ebab2bf91b353c1f7affd0196a53ac3ab6df0e3
Krishnveer Bhagod <112793546+K3-github@users.noreply.github.com>
2026-06-29 23:53:09 +0530

    Time: 4 ms (71.08%), Space: 47 MB (42.77%) - LeetHub

---
+++ 2658-maximum-number-of-fish-in-a-grid/2658-maximum-number-of-fish-in-a-grid.java
@@ -0,0 +1,34 @@
+class Solution {
+    private int solve(int row,int col,int[][] grid){
+        int n=grid.length;
+        int m=grid[0].length;
+        if(row<0 || row>=n || col<0 || col>=m || grid[row][col]==0) return 0;
+
+        int cc=grid[row][col];
+        grid[row][col]=0;
+
+        int[] drow=new int[]{0,+1,0,-1};
+        int[] dcol=new int[]{+1,0,-1,0};
+        
+        int ans=0;
+        for(int d=0;d<4;d++){
+            int nrow=row+drow[d];
+            int ncol=col+dcol[d];
+            ans+=solve(nrow,ncol,grid);
+        }
+        return cc+ans;
+    }
+    public int findMaxFish(int[][] grid) {
+        int n=grid.length;
+        int m=grid[0].length;
+        int ans=0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]>0){
+                    ans=Math.max(ans,solve(i,j,grid));
+                }
+            }
+        }
+        return ans;
+    }
+}
