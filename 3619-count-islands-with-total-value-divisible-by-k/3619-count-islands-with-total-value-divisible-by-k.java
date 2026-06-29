265a9e30a9552a7d1069804865d5d858e6d373b6
Krishnveer Bhagod <112793546+K3-github@users.noreply.github.com>
2026-06-30 00:00:10 +0530

    Time: 23 ms (38.75%), Space: 121.2 MB (16.25%) - LeetHub

--- 3619-count-islands-with-total-value-divisible-by-k/3619-count-islands-with-total-value-divisible-by-k.java
+++ 3619-count-islands-with-total-value-divisible-by-k/3619-count-islands-with-total-value-divisible-by-k.java
@@ -1,22 +1,22 @@
 class Solution {
-    private long solve(int row,int col,int[][] grid){
+    private int solve(int row,int col,int[][] grid,int k){
         int n=grid.length;
         int m=grid[0].length;
         if(row<0 || row>=n || col<0 || col>=m || grid[row][col]==0) return 0;
 
-        long cc=grid[row][col];
+        int cc=grid[row][col]%k;
         grid[row][col]=0;
 
         int[] drow=new int[]{0,+1,0,-1};
         int[] dcol=new int[]{+1,0,-1,0};
         
-        long ans=0;
+        int ans=0;
         for(int d=0;d<4;d++){
             int nrow=row+drow[d];
             int ncol=col+dcol[d];
-            ans+=solve(nrow,ncol,grid);
+            ans+=solve(nrow,ncol,grid,k)%k;
         }
-        return cc+ans;
+        return (cc+ans)%k;
     }
     public int countIslands(int[][] grid, int k) {
         int n=grid.length;
@@ -25,7 +25,7 @@ class Solution {
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(grid[i][j]>0){
-                    long val=solve(i,j,grid);
+                    int val=solve(i,j,grid,k);
                     if(val%k==0) count++;
                 }
             }
