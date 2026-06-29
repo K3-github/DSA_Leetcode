5ff30d2a6ef254424843ceb8a0c279c36db16d28
Krishnveer Bhagod <112793546+K3-github@users.noreply.github.com>
2026-06-29 23:41:38 +0530

    Time: 50 ms (23.26%), Space: 207.5 MB (15.63%) - LeetHub

---
+++ 2316-count-unreachable-pairs-of-nodes-in-an-undirected-graph/2316-count-unreachable-pairs-of-nodes-in-an-undirected-graph.java
@@ -0,0 +1,43 @@
+class Solution {
+    private int solve(int node,List<List<Integer>> graph,boolean[] vis){
+        if(vis[node]==true) return 0;
+        
+        vis[node]=true;
+        int ans=0;
+        for(int adjNode: graph.get(node)){
+            if(!vis[adjNode]){
+                ans+=solve(adjNode,graph,vis);
+            }
+        }
+        return 1+ans;
+    }
+    public long countPairs(int n, int[][] edges) {
+        List<List<Integer>> g=new ArrayList<>();
+        for(int i=0;i<n;i++){
+            g.add(new ArrayList<>());
+        }
+        for(int[] arr: edges){
+            int u=arr[0],v=arr[1];
+            g.get(u).add(v);
+            g.get(v).add(u);
+        }
+        boolean[] vis=new boolean[n];
+        List<Integer> cSize=new ArrayList<>();
+        for(int i=0;i<n;i++){
+            if(!vis[i]){
+               cSize.add(solve(i,g,vis));
+            }
+        }
+        // Collections.sort(cSize);
+        // System.out.println(cSize);
+        long currSum=0;
+        long ans=0;
+        for(int i=cSize.size()-1;i>=0;i--){
+            int currNum=cSize.get(i);
+             ans+=(currSum*currNum);
+             currSum+=currNum;
+        }
+        return ans;
+    }
+}
+
