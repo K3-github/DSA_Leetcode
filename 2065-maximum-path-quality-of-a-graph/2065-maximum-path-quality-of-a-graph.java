9d1f1d37496928415ade417fa05024bd4e84cedd
Krishnveer Bhagod <112793546+K3-github@users.noreply.github.com>
2026-07-01 03:03:03 +0530

    Time: 122 ms (44.91%), Space: 47.1 MB (81.75%) - LeetHub

--- 2065-maximum-path-quality-of-a-graph/2065-maximum-path-quality-of-a-graph.java
+++ 2065-maximum-path-quality-of-a-graph/2065-maximum-path-quality-of-a-graph.java
@@ -20,40 +20,29 @@ class Solution {
             int nextTime=time+p.time;
 
             if(nextTime<=maxTime){
-
                 int add=0;
-
                 if(vis[nextNode]==0){
                     add=values[nextNode];
                 }
-
                 vis[nextNode]++;
-
                 dfs(nextNode,nextTime,quality+add,values,vis,graph,maxTime);
-
                 vis[nextNode]--;
             }
         }
     }
     public int maximalPathQuality(int[] values,int[][] edges,int maxTime) {
         int n=values.length;
-
         List<List<Pair>> graph=new ArrayList<>();
-
         for(int i=0;i<n;i++){
             graph.add(new ArrayList<>());
         }
-
         for(int[] e:edges){
             graph.get(e[0]).add(new Pair(e[1],e[2]));
             graph.get(e[1]).add(new Pair(e[0],e[2]));
         }
-
         int[] vis=new int[n];
         vis[0]=1;
-
         dfs(0,0,values[0],values,vis,graph,maxTime);
-
         return ans;
     }
 }
