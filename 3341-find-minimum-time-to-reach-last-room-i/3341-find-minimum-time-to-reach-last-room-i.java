418a02a63f4e334b348025701e79ea250d4acdac
Krishnveer Bhagod <112793546+K3-github@users.noreply.github.com>
2026-06-30 19:52:16 +0530

    Time: 8 ms (65.25%), Space: 47.2 MB (8.9%) - LeetHub

--- 3341-find-minimum-time-to-reach-last-room-i/3341-find-minimum-time-to-reach-last-room-i.java
+++ 3341-find-minimum-time-to-reach-last-room-i/3341-find-minimum-time-to-reach-last-room-i.java
@@ -5,7 +5,7 @@ class Solution {
         PriorityQueue<int[]> pq=new PriorityQueue<>((a,b) -> a[0]-b[0]);
         pq.offer(new int[]{0,0,0});
         int[][] minTime=new int[n][m];
-        for(int i=0;i<n;i++) Arrays.fill(minTime[i],1000000);
+        for(int i=0;i<n;i++) Arrays.fill(minTime[i],Integer.MAX_VALUE);
         minTime[0][0]=0;
 
         while(pq.size()>0){
@@ -21,10 +21,12 @@ class Solution {
                 int nrow=row+drow[d];
                 int ncol=col+dcol[d];
 
-                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && minTime[nrow][ncol]==1000000){
+                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m){
                     int nextTime=Math.max(grid[nrow][ncol],currTime)+1;
-                    minTime[nrow][ncol]=nextTime;
-                    pq.offer(new int[]{nextTime,nrow,ncol});
+                    if(nextTime<minTime[nrow][ncol]){
+                        minTime[nrow][ncol]=nextTime;
+                        pq.offer(new int[]{nextTime,nrow,ncol});
+                    }
                 }
             }
         }
