2cc799ea330f4dff8a9764393e976f01513312f5
Krishnveer Bhagod <112793546+K3-github@users.noreply.github.com>
2026-07-02 18:04:54 +0530

    Time: 15 ms (68.44%), Space: 47.3 MB (29.92%) - LeetHub

--- 3286-find-a-safe-walk-through-a-grid/3286-find-a-safe-walk-through-a-grid.java
+++ 3286-find-a-safe-walk-through-a-grid/3286-find-a-safe-walk-through-a-grid.java
@@ -10,41 +10,62 @@ class Pair{
     }
 };
 class Solution {
-    public boolean findSafeWalk(List<List<Integer>> mat, int health){
-        int n=mat.size();
-        int m=mat.get(0).size();
-        int[][] dist=new int[n][m];
-        for(int i=0;i<n;i++){
-            Arrays.fill(dist[i],Integer.MAX_VALUE);
-        }
-        PriorityQueue<Pair> pq=new PriorityQueue<>((a,b)->a.unsafe-b.unsafe);
-        pq.offer(new Pair(mat.get(0).get(0),0,0));
-        dist[0][0]=mat.get(0).get(0);
+    public boolean findSafeWalk(List<List<Integer>> mat, int health) {
+         int n=mat.size();
+         int m=mat.get(0).size();
+         int[][] grid=new int[n][m];
+         for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                grid[i][j]=mat.get(i).get(j);
+            }
+         }
+         int[][] unsafes=new int[n][m];
+         for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                unsafes[i][j]=1000000000;
+            }
+         }
+         PriorityQueue<Pair> pq=new PriorityQueue<>((a,b) -> a.unsafe - b.unsafe);
+         if(grid[0][0]==1){
+            pq.offer(new Pair(1,0,0));
+            unsafes[0][0]=1;
+         }
+         else{
+            pq.offer(new Pair(0,0,0));
+            unsafes[0][0]=0;
+         }
 
-        int[] dr={0,1,0,-1};
-        int[] dc={1,0,-1,0};
-
-        while(!pq.isEmpty()){
+         while(pq.size()>0){
             Pair p=pq.poll();
             int unsafe=p.unsafe;
             int row=p.row;
             int col=p.col;
 
-            if(row==n-1 && col==m-1) return unsafe < health;
-            if(unsafe>dist[row][col])continue;
+            if(row==n-1 && col==m-1) return unsafe<health;
+            if(unsafe>unsafes[row][col]) continue;
 
-            for(int i=0;i<4;i++){
-                int nr=row+dr[i];
-                int nc=col+dc[i];
-                if(nr>=0 && nr<n && nc>=0 && nc<m){
-                    int newUnsafe =unsafe + mat.get(nr).get(nc);
-                    if(newUnsafe < dist[nr][nc]){
-                        dist[nr][nc]=newUnsafe;
-                        pq.offer(new Pair(newUnsafe,nr,nc));
+            int[] drow=new int[]{0,+1,0,-1};
+            int[] dcol=new int[]{+1,0,-1,0};
+
+            for(int d=0;d<4;d++){
+                int nrow=row+drow[d];
+                int ncol=col+dcol[d];
+                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m){
+                    if(grid[nrow][ncol]==1){
+                        if(1+unsafe<unsafes[nrow][ncol]){
+                            pq.offer(new Pair(1+unsafe,nrow,ncol));
+                            unsafes[nrow][ncol]=1+unsafe;
+                        }
+                    }
+                    else{
+                        if(unsafe<unsafes[nrow][ncol]){
+                            pq.offer(new Pair(unsafe,nrow,ncol));
+                            unsafes[nrow][ncol]=unsafe;
+                        }
                     }
                 }
             }
-        }
-        return false;
+         }
+         return false;
     }
 }
