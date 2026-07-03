b9e0d995229461f218eecb08f3af0735ae1c6d34
Krishnveer Bhagod <112793546+K3-github@users.noreply.github.com>
2026-07-03 20:00:42 +0530

    Time: 194 ms (65.76%), Space: 274.2 MB (53.69%) - LeetHub

---
+++ 0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure.java
@@ -0,0 +1,64 @@
+class Node{
+    Node[] links=new Node[26];
+    boolean flag=false;
+
+    Node(){}
+};
+class WordDictionary {
+    private static Node root;
+    public WordDictionary() {
+        root=new Node();
+    }
+    
+    public void addWord(String word) {
+        Node node=root;
+        for(char ch: word.toCharArray()){
+             if(node.links[ch-'a']==null){
+                node.links[ch-'a']=new Node();
+             }
+             node=node.links[ch-'a'];
+        }
+        node.flag=true;
+        return;
+    }
+    private boolean solve(Node node,String word,int ind){
+        if(ind>=word.length()) return node.flag;
+        char ch=word.charAt(ind);
+        if(ch=='.'){
+            boolean ans=false;
+            for(int i=0;i<26;i++){
+                if(node.links[i]!=null){
+                   ans=ans || solve(node.links[i],word,ind+1);
+                }
+            }
+            return ans;
+        }
+        if(node.links[ch-'a']==null) return false;
+        return solve(node.links[ch-'a'],word,ind+1);
+    }
+    public boolean search(String word) {
+        Node node=root;
+        for(int ind=0;ind<word.length();ind++){
+            char ch=word.charAt(ind);
+            if(ch=='.'){
+                boolean ans=false;
+                for(int i=0;i<26;i++){
+                    if(node.links[i]!=null){
+                      ans=ans || solve(node.links[i],word,ind+1);
+                    }
+                }
+                return ans;
+            }
+            if(node.links[ch-'a']==null) return false;
+            node=node.links[ch-'a'];
+        }
+        return node.flag;
+    }
+}
+
+/**
+ * Your WordDictionary object will be instantiated and called as such:
+ * WordDictionary obj = new WordDictionary();
+ * obj.addWord(word);
+ * boolean param_2 = obj.search(word);
+ */
