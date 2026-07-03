7ae7bb012e13fcd9f9489ebd8ac632371628f162
Krishnveer Bhagod <112793546+K3-github@users.noreply.github.com>
2026-07-04 01:46:16 +0530

    Time: 1 ms (99.6%), Space: 43.9 MB (76.56%) - LeetHub

--- 0139-word-break/0139-word-break.java
+++ 0139-word-break/0139-word-break.java
@@ -1,26 +1,45 @@
-class Solution {
-    HashSet<String> st=new HashSet<>();
-    private int solve(int ind,String s,int[] dp){
-        if(ind>=s.length()) return 1;
-        if(dp[ind]!=-1) return dp[ind];
+class Node{
+    Node[] links=new Node[26];
+    boolean end=false;
 
-        int ans=0;
+    Node(){}
+};
+class Solution {
+    private Node root=new Node();
+    private void insert(String word){
+        Node node=root;
+        for(char ch: word.toCharArray()){
+            if(node.links[ch-'a']==null){
+                node.links[ch-'a']=new Node();
+            }
+            node=node.links[ch-'a'];
+        }
+        node.end=true;
+    }
+    private boolean solve(int ind,String s,int[] dp){
+        if(ind>=s.length()) return true;
+        if(dp[ind]!=-1) return dp[ind]==1;
+
+        Node node=root;
+        boolean ans=false;
         for(int j=ind;j<s.length();j++){
-            String str=s.substring(ind,j+1);
-            if(st.contains(str)){
-                if(solve(j+1,s,dp)==1) ans=1;
+            char ch=s.charAt(j);
+            node=node.links[ch-'a'];
+            if(node==null) break;
+            if(node.end==true){
+                ans=ans || solve(j+1,s,dp);
             }
         }
-        return dp[ind]=ans;
+        dp[ind]=(ans==false ? 0 : 1);
+        return ans;
     }
     public boolean wordBreak(String s, List<String> wordDict) {
          for(int i=0;i<wordDict.size();i++){
-            st.add(wordDict.get(i));
+            insert(wordDict.get(i));
          }
          int n=s.length();
          int[] dp=new int[n];
          Arrays.fill(dp,-1);
-         int res=solve(0,s,dp);
-         return res==0 ? false : true;
+         return solve(0,s,dp);
     }
 }
