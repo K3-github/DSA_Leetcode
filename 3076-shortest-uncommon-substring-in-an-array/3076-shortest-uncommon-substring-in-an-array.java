28d99d81893a319232215150dcbffa50f31b998f
Krishnveer Bhagod <112793546+K3-github@users.noreply.github.com>
2026-07-04 15:05:50 +0530

    Time: 74 ms (58.72%), Space: 47.6 MB (72.95%) - LeetHub

--- 3076-shortest-uncommon-substring-in-an-array/3076-shortest-uncommon-substring-in-an-array.java
+++ 3076-shortest-uncommon-substring-in-an-array/3076-shortest-uncommon-substring-in-an-array.java
@@ -1,63 +1,67 @@
 class Node{
     Node[] links=new Node[26];
     int count=0;
-    int lastVisited=-1;
+    int lastVisInd=-1;
+    
     Node(){}
-}
-class Solution{
+};
+class Solution {
     private Node root=new Node();
-    private void insert(String word,int idx){
+    private void insert(String word,int index){
         Node node=root;
-        for(char ch:word.toCharArray()){
+        for(char ch: word.toCharArray()){
             if(node.links[ch-'a']==null){
                 node.links[ch-'a']=new Node();
             }
             node=node.links[ch-'a'];
-            if(node.lastVisited!=idx){
+            if(node.lastVisInd!=index){
                 node.count++;
-                node.lastVisited=idx;
+                node.lastVisInd=index;
             }
         }
+        return;
     }
     private boolean isValid(String word){
-        Node node=root;
-        for(char ch:word.toCharArray()){
+         Node node=root;
+         for(char ch: word.toCharArray()){
             node=node.links[ch-'a'];
         }
         return node.count==1;
     }
-    public String[] shortestSubstrings(String[] arr){
+    public String[] shortestSubstrings(String[] arr) {
         int n=arr.length;
-        for(int idx=0;idx<n;idx++){
-            HashSet<String> set=new HashSet<>();
-            String str=arr[idx];
-            for(int i=0;i<str.length();i++){
-                for(int j=i;j<str.length();j++){
-                    set.add(str.substring(i,j+1));
+        for(int index=0;index<n;index++){
+            String curr=arr[index];
+            int len=curr.length();
+            HashSet<String> st=new HashSet<>();
+            for(int start=0;start<len;start++){
+                for(int end=start;end<len;end++){
+                    String sub=curr.substring(start,end+1);
+                    if(st.contains(sub)) continue;
+                    insert(sub,index);
+                    st.add(sub);
                 }
             }
-            for(String s:set){
-                insert(s,idx);
-            }
         }
-        String[] ans=new String[n];
-        for(int idx=0;idx<n;idx++){
-            String res="";
-            for(int len=1;len<=arr[idx].length();len++){
-                for(int i=0;i+len<=arr[idx].length();i++){
-                    String sub=arr[idx].substring(i,i+len);
+        String[] answer=new String[n];
+        for(int index=0;index<n;index++){
+             String best="";
+             String curr=arr[index];
+             int len=curr.length();
+             for(int start=0;start<len;start++){
+                for(int end=start;end<len;end++){
+                    String sub=curr.substring(start,end+1);
                     if(isValid(sub)){
-                        if(res.equals("")||sub.compareTo(res)<0){
-                            res=sub;
+                        if(best.equals("")
+                            || sub.length() < best.length()
+                            || (sub.length() == best.length() && sub.compareTo(best) < 0)){
+                            best = sub;
                         }
                     }
                 }
-                if(!res.equals("")){
-                    break;
-                }
-            }
-            ans[idx]=res;
+             }
+             answer[index]=best;
         }
-        return ans;
+        return answer;
     }
 }
