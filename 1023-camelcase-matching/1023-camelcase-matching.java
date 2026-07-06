24850e3857aad2e20dda721e3e4aec1499807e4c
Krishnveer Bhagod <112793546+K3-github@users.noreply.github.com>
2026-07-06 16:33:27 +0530

    Time: 0 ms (100%), Space: 43 MB (60.62%) - LeetHub

---
+++ 1023-camelcase-matching/1023-camelcase-matching.java
@@ -0,0 +1,31 @@
+class Solution {
+    private boolean isMatch(String q,String p){
+        int n=q.length(),m=p.length();
+        int i=0,j=0;
+        while(i<n && j<m){
+            char ch1=q.charAt(i);
+            char ch2=p.charAt(j);
+            if(ch1==ch2){
+                i++;j++;
+            }
+            else{
+                if(ch1>='A' && ch1<='Z') return false;
+                i++;
+            }
+        }
+        while(i<n){
+            char ch1=q.charAt(i);
+            if(ch1>='A' && ch1<='Z') return false;
+            i++;
+        }
+        return (j==m);
+    }
+    public List<Boolean> camelMatch(String[] queries, String pattern) {
+        List<Boolean> ans=new ArrayList<>();
+        for(String q: queries){
+            if(isMatch(q,pattern)) ans.add(true);
+            else ans.add(false);
+        }
+        return ans;
+    }
+}
