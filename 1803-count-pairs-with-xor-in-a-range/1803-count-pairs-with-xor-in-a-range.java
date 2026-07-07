a3590388fe72bfe0a3984c139e151bce09adaba4
Krishnveer Bhagod <112793546+K3-github@users.noreply.github.com>
2026-07-08 01:54:44 +0530

    Time: 125 ms (14.52%), Space: 51.5 MB (70.97%) - LeetHub

---
+++ 1803-count-pairs-with-xor-in-a-range/1803-count-pairs-with-xor-in-a-range.java
@@ -0,0 +1,52 @@
+class Node{
+    Node[] links=new Node[2];
+    int count=0;
+}
+class Solution {
+    private Node root=new Node();
+    private void insert(int num){
+        Node node=root;
+        for(int i=31;i>=0;i--){
+            int bit=(num>>i)&1;
+            if(node.links[bit]==null){
+                node.links[bit]=new Node();
+            }
+            node=node.links[bit];
+            node.count++;
+        }
+    }
+    private int getPairs(int num,int limit){
+        Node node=root;
+        int ans=0;
+
+        for(int i=31;i>=0;i--){
+            if(node==null) break;
+
+            int bitNum=(num>>i)&1;
+            int bitLimit=(limit>>i)&1;
+
+            if(bitLimit==1){
+                if(node.links[bitNum]!=null){
+                    ans+=node.links[bitNum].count;
+                }
+                node=node.links[1-bitNum];
+            }
+            else{
+                node=node.links[bitNum];
+            }
+        }
+        if(node!=null) ans+=node.count;
+        return ans;
+    }
+    public int countPairs(int[] nums, int low, int high) {
+         int n=nums.length;
+         int ans=0;
+         for(int i=0;i<n;i++){
+            ans+=getPairs(nums[i],high);
+            ans-=getPairs(nums[i],low-1);
+
+            insert(nums[i]);
+         }
+         return ans;
+    }
+}
