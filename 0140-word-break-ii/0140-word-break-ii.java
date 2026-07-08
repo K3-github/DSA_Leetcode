99cfef75ac4fbe0f3499bc21c21a2a5b1e080734
Krishnveer Bhagod <112793546+K3-github@users.noreply.github.com>
2026-07-08 17:36:27 +0530

    Time: 1 ms (94.17%), Space: 43.3 MB (69.82%) - LeetHub

--- 0140-word-break-ii/0140-word-break-ii.java
+++ 0140-word-break-ii/0140-word-break-ii.java
@@ -1,16 +1,37 @@
+class Node{
+    Node[] links=new Node[26];
+    boolean end=false;
+
+    Node(){}
+};
 class Solution {
-    HashSet<String> st=new HashSet<>();
+    private Node root=new Node();
     List<String> ans=new ArrayList<>();
+    private void insert(String word){
+        Node node=root;
+        for(char ch: word.toCharArray()){
+            if(node.links[ch-'a']==null){
+                node.links[ch-'a']=new Node();
+            }
+            node=node.links[ch-'a'];
+        }
+        node.end=true;
+    }
     private void solve(int ind,List<String> curr,String s){
         if(ind==s.length()){
             String str=String.join(" ",curr);
             ans.add(str);
             return;
         }
+        Node node=root;
+        StringBuilder sub=new StringBuilder();
         for(int j=ind;j<s.length();j++){
-            String sub=s.substring(ind,j+1);
-            if(st.contains(sub)){
-                curr.add(sub);
+            char ch=s.charAt(j);
+            sub.append(ch);
+            node=node.links[ch-'a'];
+            if(node==null) break;
+            if(node.end==true){
+                curr.add(sub.toString());
                 solve(j+1,curr,s);
                 curr.remove(curr.size()-1);
             }
@@ -18,7 +39,7 @@ class Solution {
     }
     public List<String> wordBreak(String s, List<String> wordDict) {
         for(String str: wordDict){
-            st.add(str);
+            insert(str);
         }
         List<String> curr=new ArrayList<>();
         solve(0,curr,s);
