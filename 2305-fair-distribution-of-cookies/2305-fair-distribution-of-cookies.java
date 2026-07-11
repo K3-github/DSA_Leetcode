364ebb201553d006dcb7d030c0919e6c3985bbee
Krishnveer Bhagod <112793546+K3-github@users.noreply.github.com>
2026-07-11 14:36:09 +0530

    Time: 0 ms (100%), Space: 42.2 MB (97.05%) - LeetHub

--- 2305-fair-distribution-of-cookies/2305-fair-distribution-of-cookies.java
+++ 2305-fair-distribution-of-cookies/2305-fair-distribution-of-cookies.java
@@ -1,27 +1,20 @@
 class Solution {
     private boolean solve(int ind,int[] cookies,int[] children,int limit){
         if(ind==cookies.length) return true;
-
         for(int i=0;i<children.length;i++){
             if(children[i]+cookies[ind]>limit) continue;
-
             children[i]+=cookies[ind];
-
             if(solve(ind+1,cookies,children,limit)){
                 return true;
             }
-
             children[i]-=cookies[ind];
-
-            if(children[i]==0) break;
+            // if(children[i]==0) break;
         }
-
         return false;
     }
 
     public int distributeCookies(int[] cookies, int k) {
         int low=0,high=0;
-
         for(int x: cookies){
             low=Math.max(low,x);
             high+=x;
