1799e9f2e917de4a458165cbd0d73cb9799fc9f7
Krishnveer Bhagod <112793546+K3-github@users.noreply.github.com>
2026-07-11 14:50:41 +0530

    Time: 0 ms (100%), Space: 42.4 MB (85.45%) - LeetHub

--- 2305-fair-distribution-of-cookies/2305-fair-distribution-of-cookies.java
+++ 2305-fair-distribution-of-cookies/2305-fair-distribution-of-cookies.java
@@ -1,38 +1,31 @@
 class Solution {
-    private boolean solve(int ind,int[] cookies,int[] children,int limit){
+    private boolean isPossible(int ind,int[] cookies,int mx,int[] children){
         if(ind==cookies.length) return true;
+
         for(int i=0;i<children.length;i++){
-            if(children[i]+cookies[ind]>limit) continue;
+            if(children[i]+cookies[ind]>mx) continue;
             children[i]+=cookies[ind];
-            if(solve(ind+1,cookies,children,limit)){
-                return true;
-            }
+            if(isPossible(ind+1,cookies,mx,children)==true) return true;
             children[i]-=cookies[ind];
-            // if(children[i]==0) break;
         }
         return false;
     }
-
     public int distributeCookies(int[] cookies, int k) {
         int low=0,high=0;
-        for(int x: cookies){
-            low=Math.max(low,x);
-            high+=x;
+        int n=cookies.length;
+        for(int i=0;i<n;i++){
+            low=Math.max(low,cookies[i]);
+            high+=cookies[i];
         }
-
+        
         while(low<=high){
             int mid=low+(high-low)/2;
-
             int[] children=new int[k];
-
-            if(solve(0,cookies,children,mid)){
+            if(isPossible(0,cookies,mid,children)==true){
                 high=mid-1;
             }
-            else{
-                low=mid+1;
-            }
+            else low=mid+1;
         }
-
         return low;
     }
 }
