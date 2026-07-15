531cf919c488673e90af34d1f84a9b028c55bc76
Krishnveer Bhagod <112793546+K3-github@users.noreply.github.com>
2026-07-15 14:16:20 +0530

    Time: 3 ms (45.45%), Space: 44 MB (82.42%) - LeetHub

--- 2824-count-pairs-whose-sum-is-less-than-target/2824-count-pairs-whose-sum-is-less-than-target.java
+++ 2824-count-pairs-whose-sum-is-less-than-target/2824-count-pairs-whose-sum-is-less-than-target.java
@@ -1,18 +1,25 @@
 class Solution {
-    private int solve(List<Integer> nums,int limit){
-        int i=0,j=nums.size()-1;
-        int count=0;
-        while(i<j){
-             if(nums.get(i)+nums.get(j)<limit){
-                 count+=j-i;
-                 i++;
+    private int binarySearch(List<Integer> nums,int x,int ind){
+        int low=ind,high=nums.size()-1;
+        int ans=-1;
+        while(low<=high){
+             int mid=(low+high) >> 1;
+             if(nums.get(mid)<x){
+                ans=mid;
+                low=mid+1;
              }
-             else j--;
+             else high=mid-1;
         }
-        return count;
+        return ans;
     }
     public int countPairs(List<Integer> nums, int target) {
         Collections.sort(nums);
-        return solve(nums,target);
+        int n=nums.size();
+        int count=0;
+        for(int i=0;i<n;i++){
+            int lastInd=binarySearch(nums,target-nums.get(i),i+1);
+            if(lastInd!=-1) count+=lastInd-i;
+        }
+        return count;
     }
 }
