dd7744db00e5090a795e9d49655a44410ddedde2
Krishnveer Bhagod <112793546+K3-github@users.noreply.github.com>
2026-07-15 13:09:59 +0530

    Time: 33 ms (100%), Space: 86.2 MB (11.35%) - LeetHub

--- 2563-count-the-number-of-fair-pairs/2563-count-the-number-of-fair-pairs.java
+++ 2563-count-the-number-of-fair-pairs/2563-count-the-number-of-fair-pairs.java
@@ -1,39 +1,21 @@
 class Solution {
-    private int binarySearch2(int[] nums,int x,int ind){
-        int ans=-1,n=nums.length;
-        int low=ind,high=n-1;
-        while(low<=high){
-            int mid=(low+high) >> 1;
-            if(nums[mid]<=x){
-                ans=mid;
-                low=mid+1;
-            }
-            else high=mid-1;
-        }
-        return ans;
-    }
-    private int binarySearch1(int[] nums,int x,int ind){
-        int ans=-1,n=nums.length;
-        int low=ind,high=n-1;
-        while(low<=high){
-            int mid=(low+high) >> 1;
-            if(nums[mid]>=x){
-                ans=mid;
-                high=mid-1;
-            }
-            else low=mid+1;
-        }
-        return ans;
-    }
-    public long countFairPairs(int[] nums, int lower, int upper) {
-        long count=0;
+    private long countPairs(int[] nums,int limit){
         int n=nums.length;
-        Arrays.sort(nums);
-        for(int i=0;i<n;i++){
-            int start=binarySearch1(nums,lower-nums[i],i+1);
-            int end=binarySearch2(nums,upper-nums[i],i+1);
-            if(start!=-1 && end>=start) count+=end-start+1;
+        int i=0,j=n-1;
+        long count=0;
+        while(i<j){
+            if(nums[i]+nums[j]>limit){
+                j--;
+            }
+            else{
+                count+=j-i;
+                i++;
+            }
         }
         return count;
+    }
+    public long countFairPairs(int[] nums, int lower, int upper) {
+        Arrays.sort(nums);
+        return countPairs(nums,upper)-countPairs(nums,lower-1);
     }
 }
