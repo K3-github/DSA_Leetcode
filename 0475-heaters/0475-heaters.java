fdaa8cec382b850627094857d2f77ab6305c1cb4
Krishnveer Bhagod <112793546+K3-github@users.noreply.github.com>
2026-07-16 12:59:32 +0530

    Time: 18 ms (88.38%), Space: 47.9 MB (70.01%) - LeetHub

--- 0475-heaters/0475-heaters.java
+++ 0475-heaters/0475-heaters.java
@@ -1,29 +1,15 @@
 class Solution {
-    private int binarySearch(int[] h,int x){
-        int low=0,high=h.length-1;
-        while(low<=high){
-            int mid=(low+high) >> 1;
-            if(h[mid]>=x) high=mid-1;
-            else low=mid+1;
-        }
-        return low;
-    }
     public int findRadius(int[] houses, int[] heaters) {
-        int n=houses.length;
+        Arrays.sort(houses);
         Arrays.sort(heaters);
+        int j=0;
         int ans=0;
-        for(int i=0;i<n;i++){
-            int ind=binarySearch(heaters,houses[i]);
-            int left=Integer.MAX_VALUE;
-            int right=Integer.MAX_VALUE;
-
-            if(ind>0){
-                left=houses[i]-heaters[ind-1];
+        for(int i=0;i<houses.length;i++){
+            while(j+1<heaters.length &&
+                  Math.abs(heaters[j+1]-houses[i])<=Math.abs(heaters[j]-houses[i])){
+                j++;
             }
-            if(ind<heaters.length){
-                right=heaters[ind]-houses[i];
-            }
-            ans=Math.max(ans,Math.min(left,right));
+            ans=Math.max(ans,Math.abs(heaters[j]-houses[i]));
         }
         return ans;
     }
