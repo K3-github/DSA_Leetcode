64927d650083ee4643fc4acfd50881aad6b8076b
Krishnveer Bhagod <112793546+K3-github@users.noreply.github.com>
2026-07-20 18:38:47 +0530

    Time: 107 ms (7.53%), Space: 57.1 MB (41.14%) - LeetHub

--- 0851-loud-and-rich/0851-loud-and-rich.java
+++ 0851-loud-and-rich/0851-loud-and-rich.java
@@ -1,18 +1,13 @@
 class Solution {
-    int ans;
-    int mnQ;
-    private void solve(int node,List<List<Integer>> graph,boolean[] vis,int[] quiet){
+    private int solve(int node,List<List<Integer>> graph,boolean[] vis,int[] quiet){
          vis[node]=true;
-
-         if(quiet[node]<mnQ){
-            mnQ=quiet[node];
-            ans=node;
-         }
+         int ans=500;
          for(Integer adjNode: graph.get(node)){
             if(!vis[adjNode]){
-                solve(adjNode,graph,vis,quiet);
+                ans=Math.min(ans,solve(adjNode,graph,vis,quiet));
             }
          }
+         return Math.min(ans,quiet[node]);
     }
     public int[] loudAndRich(int[][] richer, int[] quiet) {
         List<List<Integer>> graph=new ArrayList<>();
@@ -23,13 +18,15 @@ class Solution {
         for(int[] e: richer){
             graph.get(e[1]).add(e[0]);
         }
+        HashMap<Integer,Integer> index=new HashMap<>();
+        for(int i=0;i<n;i++){
+            index.put(quiet[i],i);
+        }
         int[] answer=new int[n];
         for(int i=0;i<n;i++){
             boolean[] vis=new boolean[n];
-            ans=i;
-            mnQ=quiet[i];
-            solve(i,graph,vis,quiet);
-            answer[i]=ans;
+            int mnQuiet=solve(i,graph,vis,quiet);
+            answer[i]=index.get(mnQuiet);
         }
         return answer;
     }
