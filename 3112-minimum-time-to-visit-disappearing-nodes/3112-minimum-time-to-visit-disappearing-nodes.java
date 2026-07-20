8810119b3b64229f854fa69c97cff9de75b2fbea
Krishnveer Bhagod <112793546+K3-github@users.noreply.github.com>
2026-07-21 01:40:49 +0530

    Time: 73 ms (73.74%), Space: 201.3 MB (64.65%) - LeetHub

---
+++ 3112-minimum-time-to-visit-disappearing-nodes/3112-minimum-time-to-visit-disappearing-nodes.java
@@ -0,0 +1,53 @@
+class Pair{
+    int time;
+    int node;
+
+    Pair(int time,int node){
+        this.time=time;
+        this.node=node;
+    }
+};
+class Solution {
+    int INF=Integer.MAX_VALUE;
+    private List<List<Pair>> createGraph(int n,int[][] edges){
+       List<List<Pair>> graph=new ArrayList<>();
+       for(int i=0;i<n;i++){
+          graph.add(new ArrayList<>());
+       }
+       for(int[] e: edges){
+          int u=e[0],v=e[1],time=e[2];
+          graph.get(u).add(new Pair(time,v));
+          graph.get(v).add(new Pair(time,u));
+       }
+       return graph;
+    }
+    public int[] minimumTime(int n, int[][] edges, int[] disappear) {
+        List<List<Pair>> graph=createGraph(n,edges);
+        PriorityQueue<Pair> pq=new PriorityQueue<>((a,b) -> a.time - b.time);
+        int[] minTime=new int[n];
+        Arrays.fill(minTime,INF);
+        minTime[0]=0;
+        pq.offer(new Pair(0,0));
+
+        while(pq.size()>0){
+            Pair p=pq.poll();
+            int currTime=p.time;
+            int currNode=p.node;
+            
+            if(currTime>=disappear[currNode] || currTime>minTime[currNode]) continue;
+
+            for(Pair pr: graph.get(currNode)){
+                 int childNode=pr.node;
+                 int reqTime=pr.time;
+                 if(reqTime+currTime<minTime[childNode] && reqTime+currTime<disappear[childNode]){
+                    minTime[childNode]=reqTime+currTime;
+                    pq.offer(new Pair(minTime[childNode],childNode));
+                 }
+            }
+        }
+        for(int i=0;i<n;i++){
+            if(minTime[i]>=disappear[i]) minTime[i]=-1;
+        }
+        return minTime;
+    }
+}
