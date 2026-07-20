8f818ae5f244593d0b6e2a717db7da3dc54afe3d
Krishnveer Bhagod <112793546+K3-github@users.noreply.github.com>
2026-07-20 21:17:21 +0530

    Time: 71 ms (73.24%), Space: 73.6 MB (98.83%) - LeetHub

--- 2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph/2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph.java
+++ 2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph/2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph.java
@@ -1,10 +1,10 @@
 class Solution {
-    private void dfs(int node,List<List<Integer>> graph,List<Integer> ancestors,boolean[] vis){
+    private void dfs(int node,int start,List<List<Integer>> graph,List<Integer> ancestors,boolean[] vis){
         if(vis[node]) return;
         vis[node]=true;
-        ancestors.add(node);
+        if(node!=start) ancestors.add(node);
         for(Integer adjNode: graph.get(node)){
-            if(!vis[adjNode])dfs(adjNode,graph,ancestors,vis);
+            if(!vis[adjNode])dfs(adjNode,start,graph,ancestors,vis);
         }
     }
     public List<List<Integer>> getAncestors(int n, int[][] edges) {
@@ -20,8 +20,7 @@ class Solution {
         for(int i=0;i<n;i++){
             List<Integer> ancestors=new ArrayList<>();
             boolean[] vis=new boolean[n];
-            dfs(i,graph,ancestors,vis);
-            ancestors.remove(0);
+            dfs(i,i,graph,ancestors,vis);
             Collections.sort(ancestors);
             ans.add(new ArrayList<>(ancestors));
         }
