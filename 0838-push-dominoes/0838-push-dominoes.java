e09d0a74a17315b71224b1103963c2476f97011e
Krishnveer Bhagod <112793546+K3-github@users.noreply.github.com>
2026-07-23 01:59:14 +0530

    Time: 10 ms (86.67%), Space: 47.7 MB (89.74%) - LeetHub

--- 0838-push-dominoes/0838-push-dominoes.java
+++ 0838-push-dominoes/0838-push-dominoes.java
@@ -1,25 +1,26 @@
 class Solution {
     public String pushDominoes(String dominoes) {
-        char[] arr = dominoes.toCharArray();
-        int n = arr.length;
-        int left = -1;
-        for (int right = 0; right <= n; right++) {
-            if (right != n && arr[right] == '.') continue;
-            char leftChar = (left == -1) ? 'L' : arr[left];
-            char rightChar = (right == n) ? 'R' : arr[right];
-            if (leftChar == rightChar) {
-                for (int k = left + 1; k < right; k++)
-                    arr[k] = leftChar;
+        char[] arr=dominoes.toCharArray();
+        int left=-1;
+        int n=arr.length;
+        for(int right=0;right<=n;right++){
+            if(right!=n && arr[right]=='.') continue;
+            char leftChar=(left==-1 ? 'L' : arr[left]);
+            char rightChar=(right==n ? 'R' : arr[right]);
 
-            } else if (leftChar == 'R' && rightChar == 'L') {
-                int i = left + 1;
-                int j = right - 1;
-                while (i < j) {
-                    arr[i++] = 'R';
-                    arr[j--] = 'L';
+            if(leftChar==rightChar){
+                for(int k=left+1;k<right;k++){
+                    arr[k]=leftChar;
                 }
             }
-            left = right;
+            else if(leftChar=='R' && rightChar=='L'){
+                  int i=left+1,j=right-1;
+                  while(i<j){
+                    arr[i++]=leftChar;
+                    arr[j--]=rightChar;
+                  }
+            }
+            left=right;
         }
         return new String(arr);
     }
