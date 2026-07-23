fc10a3995530dcbf50d7ff0e78f1f8350187d3bb
Krishnveer Bhagod <112793546+K3-github@users.noreply.github.com>
2026-07-24 01:34:55 +0530

    Time: 4 ms (95.79%), Space: 43.5 MB (67.65%) - LeetHub

---
+++ 0763-partition-labels/0763-partition-labels.java
@@ -0,0 +1,22 @@
+class Solution {
+    public List<Integer> partitionLabels(String s) {
+        int n=s.length();
+        int[] lastIndex=new int[26];
+        for(int i=0;i<n;i++){
+            char ch=s.charAt(i);
+            lastIndex[ch-'a']=i;
+        }
+        int left=0;
+        int mxReach=-1;
+        List<Integer> ans=new ArrayList<>();
+        for(int right=0;right<n;right++){
+            char ch=s.charAt(right);
+            mxReach=Math.max(mxReach,lastIndex[ch-'a']);
+            if(right==mxReach){
+               ans.add(right-left+1);
+               left=right+1;
+            }
+        }
+        return ans;
+    }
+}
