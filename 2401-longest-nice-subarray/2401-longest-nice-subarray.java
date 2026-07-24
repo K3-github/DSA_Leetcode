5f2d953c0c8ae8c7a8e9947e194a8b968112097c
Krishnveer Bhagod <112793546+K3-github@users.noreply.github.com>
2026-07-24 17:53:53 +0530

    Time: 177 ms (5.02%), Space: 81.3 MB (62.19%) - LeetHub

---
+++ 2401-longest-nice-subarray/2401-longest-nice-subarray.java
@@ -0,0 +1,37 @@
+class Solution {
+    private boolean isValid(int[] freq, int ele) {
+        for (int i = 31; i >= 0; i--) {
+            int bit = (ele >> i) & 1;
+            if (bit == 1 && freq[i] > 0) return false;
+        }
+        return true;
+    }
+    private void removeLeft(int[] freq, int leftEle) {
+        for (int i = 31; i >= 0; i--) {
+            int bit = (leftEle >> i) & 1;
+            if (bit == 1) freq[i]--;
+        }
+    }
+    private void addRight(int[] freq, int rightEle) {
+        for (int i = 31; i >= 0; i--) {
+            int bit = (rightEle >> i) & 1;
+            if (bit == 1) freq[i]++;
+        }
+    }
+    public int longestNiceSubarray(int[] nums) {
+        int left = 0, right = 0, ans = 0;
+        int[] freq = new int[32];
+        int n = nums.length;
+
+        while (right < n) {
+            while (left < n && !isValid(freq, nums[right])) {
+                removeLeft(freq, nums[left]);
+                left++;
+            }
+            addRight(freq, nums[right]);
+            ans = Math.max(ans, right - left + 1);
+            right++;
+        }
+        return ans;
+    }
+}
