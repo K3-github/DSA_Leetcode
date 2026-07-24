7de965b18c696c7d1c9584705e8d58d7bc7ec45c
Krishnveer Bhagod <112793546+K3-github@users.noreply.github.com>
2026-07-24 15:56:58 +0530

    Time: 55 ms (54.96%), Space: 99.5 MB (23%) - LeetHub

--- 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.java
+++ 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.java
@@ -6,18 +6,16 @@ class Solution {
         while(right<nums.length){
               mp.put(nums[right],mp.getOrDefault(nums[right],0)+1);
               sum+=nums[right];
-              if(right-left+1<k){
-                right++;
-              }
-              else if(right-left+1==k){
+              if(right-left+1==k){
                 if(mp.size()==k){
                     ans=Math.max(ans,sum);
                 }
                 sum-=nums[left];
                 mp.put(nums[left],mp.get(nums[left])-1);
                 if(mp.get(nums[left])==0) mp.remove(nums[left]);
-                left++;right++;
+                left++;
               }
+              right++;
         }
         return ans;
     }
