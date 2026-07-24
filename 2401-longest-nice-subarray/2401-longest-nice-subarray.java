78993f68bf26d732951fe5608740854df23ecb4d
Krishnveer Bhagod <112793546+K3-github@users.noreply.github.com>
2026-07-24 17:57:40 +0530

    Time: 176 ms (5.02%), Space: 81.3 MB (41.94%) - LeetHub

--- 2401-longest-nice-subarray/2401-longest-nice-subarray.java
+++ 2401-longest-nice-subarray/2401-longest-nice-subarray.java
@@ -1,28 +1,26 @@
 class Solution {
-    private boolean isValid(int[][] freq,int ele){
+    private boolean isValid(int[] freq,int ele){
         for(int i=31;i>=0;i--){
             int bit=(ele>>i)&1;
-            if(bit==1 && freq[i][0]==1 && freq[i][1]>0) return false;
+            if(bit==1 && freq[i]>0) return false;
         }
         return true;
     }
-    private void removeLeft(int[][] freq,int leftEle){
+    private void removeLeft(int[] freq,int leftEle){
         for(int i=31;i>=0;i--){
             int bit=(leftEle>>i)&1;
-            if(bit==1) freq[i][1]--;
+            if(bit==1) freq[i]--;
         }
     }
-    private void addRight(int[][] freq,int rightEle){
+    private void addRight(int[] freq,int rightEle){
         for(int i=31;i>=0;i--){
             int bit=(rightEle>>i)&1;
-            if(bit==1){
-              freq[i]=new int[]{bit,freq[i][1]+1};
-            }
+            if(bit==1) freq[i]++;
         }
     }
     public int longestNiceSubarray(int[] nums) {
         int left=0,right=0,ans=0;
-        int[][] freq=new int[32][2];
+        int[] freq=new int[32];
         int n=nums.length;
         while(right<n){
             while(left<n && !isValid(freq,nums[right])){
