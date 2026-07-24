5eadda87a4eeae4ce991add4c210f4cd8b2816fa
Krishnveer Bhagod <112793546+K3-github@users.noreply.github.com>
2026-07-24 17:56:38 +0530

    Time: 198 ms (5.02%), Space: 86.7 MB (8.42%) - LeetHub

--- 2401-longest-nice-subarray/2401-longest-nice-subarray.java
+++ 2401-longest-nice-subarray/2401-longest-nice-subarray.java
@@ -1,35 +1,36 @@
 class Solution {
-    private boolean isValid(int[] freq, int ele) {
-        for (int i = 31; i >= 0; i--) {
-            int bit = (ele >> i) & 1;
-            if (bit == 1 && freq[i] > 0) return false;
+    private boolean isValid(int[][] freq,int ele){
+        for(int i=31;i>=0;i--){
+            int bit=(ele>>i)&1;
+            if(bit==1 && freq[i][0]==1 && freq[i][1]>0) return false;
         }
         return true;
     }
-    private void removeLeft(int[] freq, int leftEle) {
-        for (int i = 31; i >= 0; i--) {
-            int bit = (leftEle >> i) & 1;
-            if (bit == 1) freq[i]--;
+    private void removeLeft(int[][] freq,int leftEle){
+        for(int i=31;i>=0;i--){
+            int bit=(leftEle>>i)&1;
+            if(bit==1) freq[i][1]--;
         }
     }
-    private void addRight(int[] freq, int rightEle) {
-        for (int i = 31; i >= 0; i--) {
-            int bit = (rightEle >> i) & 1;
-            if (bit == 1) freq[i]++;
+    private void addRight(int[][] freq,int rightEle){
+        for(int i=31;i>=0;i--){
+            int bit=(rightEle>>i)&1;
+            if(bit==1){
+              freq[i]=new int[]{bit,freq[i][1]+1};
+            }
         }
     }
     public int longestNiceSubarray(int[] nums) {
-        int left = 0, right = 0, ans = 0;
-        int[] freq = new int[32];
-        int n = nums.length;
-
-        while (right < n) {
-            while (left < n && !isValid(freq, nums[right])) {
-                removeLeft(freq, nums[left]);
-                left++;
+        int left=0,right=0,ans=0;
+        int[][] freq=new int[32][2];
+        int n=nums.length;
+        while(right<n){
+            while(left<n && !isValid(freq,nums[right])){
+                 removeLeft(freq,nums[left]);
+                 left++;
             }
-            addRight(freq, nums[right]);
-            ans = Math.max(ans, right - left + 1);
+            addRight(freq,nums[right]);
+            ans=Math.max(ans,right-left+1);
             right++;
         }
         return ans;
