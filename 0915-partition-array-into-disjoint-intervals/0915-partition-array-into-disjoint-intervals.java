029a596cd49ec7f306b0ddb0230b173c62dad347
Krishnveer Bhagod <112793546+K3-github@users.noreply.github.com>
2026-07-25 18:05:49 +0530

    Time: 3 ms (89.74%), Space: 86.7 MB (40.47%) - LeetHub

--- 0915-partition-array-into-disjoint-intervals/0915-partition-array-into-disjoint-intervals.java
+++ 0915-partition-array-into-disjoint-intervals/0915-partition-array-into-disjoint-intervals.java
@@ -6,13 +6,11 @@ class Solution {
 
         for (int i = 1; i < nums.length; i++) {
             currMax = Math.max(currMax, nums[i]);
-
-            if (nums[i] < leftMax) {
+            if(nums[i] < leftMax) {
                 partitionIdx = i;
                 leftMax = currMax;
             }
         }
-
         return partitionIdx + 1;
     }
 }
