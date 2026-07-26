308744652693e6a2b9e7ab0adbf6c5aa6fc41aae
Krishnveer Bhagod <112793546+K3-github@users.noreply.github.com>
2026-07-26 14:58:33 +0530

    Time: 103 ms (75.41%), Space: 101.5 MB (75.41%) - LeetHub

--- 2607-make-k-subarray-sums-equal/2607-make-k-subarray-sums-equal.java
+++ 2607-make-k-subarray-sums-equal/2607-make-k-subarray-sums-equal.java
@@ -1,58 +1,23 @@
-class DSU{
-    List<Integer> par=new ArrayList<>();
-    List<Integer> size=new ArrayList<>();
-
-    DSU(int n){
-        for(int i=0;i<n;i++){
-            par.add(i);
-            size.add(1);
-        }
-    }
-    int findUlp(int node){
-        if(par.get(node)==node) return node;
-        int ulp=findUlp(par.get(node));
-        par.set(node,ulp);
-        return par.get(node);
-    }
-    void unionBySize(int u,int v){
-        int ulp_u=findUlp(u);
-        int ulp_v=findUlp(v);
-        if(ulp_u==ulp_v) return;
-        else if(size.get(ulp_u)<size.get(ulp_v)){
-            size.set(ulp_v,size.get(ulp_u)+size.get(ulp_v));
-            par.set(ulp_u,ulp_v);
-        }
-        else{
-            size.set(ulp_u,size.get(ulp_u)+size.get(ulp_v));
-            par.set(ulp_v,ulp_u);
-        }
-    }
-}
 class Solution {
+    private int gcd(int a, int b) {
+       if(b==0) return a;
+       return gcd(b,a%b);
+    }
     public long makeSubKSumEqual(int[] arr, int k) {
-        int n=arr.length;
-        DSU ds=new DSU(n);
-        for(int i=0;i<n;i++){
-            int u=i,v=(i+k)%n;
-            ds.unionBySize(u,v);
-        }
-        HashMap<Integer,List<Integer>> mp=new HashMap<>();
-        for(int i=0;i<n;i++){
-            int ulp=ds.findUlp(i);
-            List<Integer> li=mp.get(ulp);
-            mp.computeIfAbsent(ulp, x -> new ArrayList<>()).add(arr[i]);
-        }
-        long ans=0;
-        for(Map.Entry<Integer,List<Integer>> e: mp.entrySet()){
-            List<Integer> values=e.getValue();
-            Collections.sort(values);
-            int size=values.size();
-            int mid=values.get(size/2);
-            long cost=0;
-            for(int i=0;i<size;i++){
-                cost+=Math.abs(values.get(i)-mid);
+        int n = arr.length;
+        int g = gcd(n, k);
+
+        long ans = 0;
+        for (int i = 0; i < g; i++) {
+            List<Integer> values = new ArrayList<>();
+            for (int j = i; j < n; j += g) {
+                values.add(arr[j]);
             }
-            ans+=cost;
+            Collections.sort(values);
+            int median = values.get(values.size() / 2);
+            for (int x : values) {
+                ans += Math.abs(x - median);
+            }
         }
         return ans;
     }
