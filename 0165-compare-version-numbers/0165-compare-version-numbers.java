5a0b93a1c48981eca88ff18c6c37b4ced96fa713
Krishnveer Bhagod <112793546+K3-github@users.noreply.github.com>
2026-07-26 18:05:02 +0530

    Time: 0 ms (100%), Space: 42.9 MB (61.63%) - LeetHub

--- 0165-compare-version-numbers/0165-compare-version-numbers.java
+++ 0165-compare-version-numbers/0165-compare-version-numbers.java
@@ -3,7 +3,7 @@ class Solution {
         int i=0,j=0;
         int n=version1.length();
         int m=version2.length();
-        while(i<n && j<m){
+        while(i<n || j<m){
             int v1=0;
             while(i<n && version1.charAt(i)!='.'){
                 int dig=version1.charAt(i)-'0';
@@ -19,26 +19,6 @@ class Solution {
             if(v1<v2) return -1;
             else if(v1>v2) return 1;
             i++;j++;
-        }
-        while(i<n){
-            int v1=0;
-            while(i<n && version1.charAt(i)!='.'){
-                int dig=version1.charAt(i)-'0';
-                v1=v1*10+dig;
-                i++;
-            }
-            if(v1>0) return 1;
-            i++;
-        }
-        while(j<m){
-            int v2=0;
-            while(j<m && version2.charAt(j)!='.'){
-                int dig=version2.charAt(j)-'0';
-                v2=v2*10+dig;
-                j++;
-            }
-            if(v2>0) return -1;
-            j++;
         }
         return 0;
     }
