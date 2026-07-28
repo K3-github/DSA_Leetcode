cfaf1cebed4b19f4669d109c2e1d18a44b54b96c
Krishnveer Bhagod <112793546+K3-github@users.noreply.github.com>
2026-07-28 14:25:32 +0530

    Time: 13 ms (11.04%), Space: 47.2 MB (15.33%) - LeetHub

--- 0654-maximum-binary-tree/0654-maximum-binary-tree.java
+++ 0654-maximum-binary-tree/0654-maximum-binary-tree.java
@@ -14,21 +14,24 @@
  * }
  */
 class Solution {
-    private TreeNode solve(int[] nums,int low,int high){
-        if(low>high) return null;
-        int mxValue=-1,mxValueInd=0;
-        for(int i=low;i<=high;i++){
-            if(mxValue<nums[i]){
-                mxValue=nums[i];
-                mxValueInd=i;
-            }
-        }
-        TreeNode root=new TreeNode(nums[mxValueInd]);
-        root.left=solve(nums,low,mxValueInd-1);
-        root.right=solve(nums,mxValueInd+1,high);
-        return root;
-    }
     public TreeNode constructMaximumBinaryTree(int[] nums) {
-        return solve(nums,0,nums.length-1);
+        Stack<TreeNode> st=new Stack<>();
+        int n=nums.length;
+        for(int i=0;i<n;i++){
+            TreeNode node=new TreeNode(nums[i]);
+            if(st.size()>0){
+                while(st.size()>0 && st.peek().val<nums[i]){
+                    node.left = st.pop();
+                }
+                if(st.size()>0){
+                    st.peek().right=node;
+                }
+            }
+            st.push(node);
+        }
+        while (st.size() > 1) {
+            st.pop();
+        }
+        return st.peek();
     }
 }
