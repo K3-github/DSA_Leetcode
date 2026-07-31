d0bd0d6f67690c8fba14abdfba4745d2a8916ec2
Krishnveer Bhagod <112793546+K3-github@users.noreply.github.com>
2026-08-01 03:08:42 +0530

    Time: 6 ms (63.79%), Space: 66.2 MB (96.55%) - LeetHub

--- 2673-make-costs-of-paths-equal-in-a-binary-tree/2673-make-costs-of-paths-equal-in-a-binary-tree.java
+++ 2673-make-costs-of-paths-equal-in-a-binary-tree/2673-make-costs-of-paths-equal-in-a-binary-tree.java
@@ -1,20 +1,18 @@
 class Solution {
     int ans=0;
-    private int solve(int root,int[] cost,int n,int[] dp){
-        if(root>=n) return 0;
-        if(dp[root]!=-1) return dp[root];
+    private int solve(int root,int[] cost,int n){
+        if(root>n) return 0;
 
-        int left=solve(2*root+1,cost,n,dp);
-        int right=solve(2*root+2,cost,n,dp);
+        int left=solve(2*root,cost,n);
+        int right=solve(2*root+1,cost,n);
         
         int mx=Math.max(left,right);
         ans+=2*mx-left-right;
-        return dp[root]=cost[root]+mx;
+        return cost[root-1]+mx;
     }
     public int minIncrements(int n, int[] cost) {
         int[] dp=new int[n];
-        Arrays.fill(dp,-1);
-        solve(0,cost,n,dp);
+        solve(1,cost,n);
         return ans;
     }
 }
