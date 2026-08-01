86389785a36edb7054b102a005041bdb179af0b1
Krishnveer Bhagod <112793546+K3-github@users.noreply.github.com>
2026-08-01 13:47:02 +0530

    Time: 0 ms (100%), Space: 46.8 MB (6.78%) - LeetHub

--- 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.java
+++ 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.java
@@ -1,43 +1,17 @@
-/*
-// Definition for a Node.
-class Node {
-    public int val;
-    public Node left;
-    public Node right;
-    public Node next;
-
-    public Node() {}
-    
-    public Node(int _val) {
-        val = _val;
-    }
-
-    public Node(int _val, Node _left, Node _right, Node _next) {
-        val = _val;
-        left = _left;
-        right = _right;
-        next = _next;
-    }
-};
-*/
-
 class Solution {
     public Node connect(Node root) {
-        Queue<Node> q=new LinkedList<>();
-        if(root==null) return root;
-        q.offer(root);
-
-        while(!q.isEmpty()){
-            int size=q.size();
-            Node prev=null;
-            for(int i=0;i<size;i++){
-               Node curr=q.poll();
-               if(prev!=null) prev.next=curr;
-               prev=curr;
-               if(curr.left!=null) q.offer(curr.left);
-               if(curr.right!=null) q.offer(curr.right);
+        if (root == null) return null;
+        Node leftMost = root;
+        while (leftMost.left != null) {
+            Node curr = leftMost;
+            while (curr != null) {
+                curr.left.next = curr.right;
+                if (curr.next != null) {
+                    curr.right.next = curr.next.left;
+                }
+                curr = curr.next;
             }
-            prev.next=null;
+            leftMost = leftMost.left;
         }
         return root;
     }
