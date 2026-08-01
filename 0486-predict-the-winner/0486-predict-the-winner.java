aac32496f131f46e8df9796664f7c894c62e8daf
Krishnveer Bhagod <112793546+K3-github@users.noreply.github.com>
2026-08-01 12:45:34 +0530

    Time: 268 ms (5.13%), Space: 46.4 MB (5.44%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.java
+++ 0486-predict-the-winner/0486-predict-the-winner.java
@@ -1,14 +1,40 @@
 class Solution {
-    private int solve(int[] nums, int left, int right) {
-        if (left > right) return 0;
 
-        int pickLeft = nums[left] - solve(nums, left + 1, right);
-        int pickRight = nums[right] - solve(nums, left, right - 1);
+    private int[] solve(int[] nums, int start, int end, int turn) {
 
-        return Math.max(pickLeft, pickRight);
+        if (start > end)
+            return new int[]{0, 0};
+
+        if (turn == 0) { // Player 1's turn
+
+            int[] left = solve(nums, start + 1, end, 1);
+            int[] right = solve(nums, start, end - 1, 1);
+
+            left = new int[]{left[0] + nums[start], left[1]};
+            right = new int[]{right[0] + nums[end], right[1]};
+
+            if (left[0] > right[0])
+                return left;
+            else
+                return right;
+
+        } else { // Player 2's turn
+
+            int[] left = solve(nums, start + 1, end, 0);
+            int[] right = solve(nums, start, end - 1, 0);
+
+            left = new int[]{left[0], left[1] + nums[start]};
+            right = new int[]{right[0], right[1] + nums[end]};
+
+            if (left[1] > right[1])
+                return left;
+            else
+                return right;
+        }
     }
 
     public boolean predictTheWinner(int[] nums) {
-        return solve(nums, 0, nums.length - 1) >= 0;
+        int[] ans = solve(nums, 0, nums.length - 1, 0);
+        return ans[0] >= ans[1];
     }
 }
