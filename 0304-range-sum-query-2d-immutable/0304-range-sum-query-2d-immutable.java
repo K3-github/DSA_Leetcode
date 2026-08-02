a0293c4ab87a84a4447d040c8b6c146551b3c1c4
Krishnveer Bhagod <112793546+K3-github@users.noreply.github.com>
2026-08-03 02:03:29 +0530

    Time: 101 ms (81.11%), Space: 141.5 MB (80.9%) - LeetHub

--- 0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.java
+++ 0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.java
@@ -1,31 +1,27 @@
 class NumMatrix {
-    
+
     int[][] psum;
-    public NumMatrix(int[][] mat) {
-        int n=mat.length;
-        int m=mat[0].length;
-        psum=new int[n][m];
-        for(int i=0;i<n;i++){
-          psum[i][0]=mat[i][0];
-        }
-        for(int i=0;i<n;i++){
-            for(int j=1;j<m;j++){
-                psum[i][j]=psum[i][j-1]+mat[i][j];
+
+    public NumMatrix(int[][] matrix) {
+        int n = matrix.length;
+        int m = matrix[0].length;
+
+        psum = new int[n + 1][m + 1];
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                psum[i][j] = matrix[i - 1][j - 1]
+                           + psum[i - 1][j]
+                           + psum[i][j - 1]
+                           - psum[i - 1][j - 1];
             }
         }
     }
-    
+
     public int sumRegion(int row1, int col1, int row2, int col2) {
-        int ans=0;
-        for(int i=row1;i<=row2;i++){
-            ans+=psum[i][col2]-(col1>0 ? psum[i][col1-1] : 0);
-        }
-        return ans;
+        return psum[row2 + 1][col2 + 1]
+             - psum[row1][col2 + 1]
+             - psum[row2 + 1][col1]
+             + psum[row1][col1];
     }
 }
-
-/**
- * Your NumMatrix object will be instantiated and called as such:
- * NumMatrix obj = new NumMatrix(matrix);
- * int param_1 = obj.sumRegion(row1,col1,row2,col2);
- */
