c47cc426f8ed0df581cc0a4a9ab2eef4b0934d46
Krishnveer Bhagod <112793546+K3-github@users.noreply.github.com>
2026-08-02 16:43:42 +0530

    Time: 70 ms (60.48%), Space: 62.3 MB (48.39%) - LeetHub

--- 1690-stone-game-vii/1690-stone-game-vii.java
+++ 1690-stone-game-vii/1690-stone-game-vii.java
@@ -1,14 +1,14 @@
 class Solution {
     int[] psum;
-    private int solve(int left,int right,int[] stones,int[][] dp){
+    private int solve(int left,int right,int[][] dp){
         if(left==right) return 0;
         if(dp[left][right]!=-1) return dp[left][right]; 
         
         int leftScore=psum[right]-psum[left];
         int rightScore=psum[right-1]-(left>0 ? psum[left-1] : 0);
 
-        int pickLeft=leftScore-solve(left+1,right,stones,dp);
-        int pickRight=rightScore-solve(left,right-1,stones,dp);
+        int pickLeft=leftScore-solve(left+1,right,dp);
+        int pickRight=rightScore-solve(left,right-1,dp);
 
         return dp[left][right]=Math.max(pickLeft,pickRight);
     }
@@ -23,6 +23,6 @@ class Solution {
         for(int i=1;i<n;i++){
             psum[i]=stones[i]+psum[i-1];
         }
-        return solve(0,n-1,stones,dp);
+        return solve(0,n-1,dp);
     }
 }
