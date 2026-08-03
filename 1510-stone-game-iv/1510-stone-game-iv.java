f50787c95534d5f6cbe902358481133514ee7b9a
Krishnveer Bhagod <112793546+K3-github@users.noreply.github.com>
2026-08-04 00:57:08 +0530

    Time: 39 ms (38.78%), Space: 51 MB (30.61%) - LeetHub

---
+++ 1510-stone-game-iv/1510-stone-game-iv.java
@@ -0,0 +1,14 @@
+class Solution {
+    private int solve(int n,int[] dp){
+        if(dp[n]!=-1) return dp[n];
+        for(int i=1;i*i<=n;i++){
+            if(solve(n-i*i,dp)==0) return dp[n]=1;
+        }
+        return dp[n]=0;
+    }
+    public boolean winnerSquareGame(int n) {
+        int[] dp=new int[n+1];
+        Arrays.fill(dp,-1);
+        return solve(n,dp)==1 ? true : false;
+    }
+}
