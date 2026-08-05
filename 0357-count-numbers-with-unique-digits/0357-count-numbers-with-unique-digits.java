1ede0b4b5af5e78ba116320d91b8aa31b03f2667
Krishnveer Bhagod <112793546+K3-github@users.noreply.github.com>
2026-08-05 22:53:32 +0530

    Time: 36 ms (8.61%), Space: 42.2 MB (20.71%) - LeetHub

--- 0357-count-numbers-with-unique-digits/0357-count-numbers-with-unique-digits.java
+++ 0357-count-numbers-with-unique-digits/0357-count-numbers-with-unique-digits.java
@@ -1,25 +1,21 @@
 class Solution {
-    private int solve(int pos,boolean tight,int mask,int n){
+    private int solve(int pos,int mask,int n){
         if(pos==n) return 1;
-        
+    
         int ans=0;
         for(int digit=0;digit<=9;digit++){
-            boolean newTight=tight && (digit==9);
-
             if(mask==0 && digit==0){
-               ans+=solve(pos+1,newTight,mask,n);
+               ans+=solve(pos+1,mask,n);
             }
             else{
                 if((mask & (1<<digit))!=0) continue;
-
                 int newMask=mask | (1<<digit);
-                ans+=solve(pos+1,newTight,newMask,n);
+                ans+=solve(pos+1,newMask,n);
             }
         }
         return ans;
     }
     public int countNumbersWithUniqueDigits(int n) {
-        int mx=(int)Math.pow(10,n)-1;
-        return solve(0,true,0,n);
+        return solve(0,0,n);
     }
 }
