7ce6e05e8d964ea6441387cf257a3b2199f516fb
Krishnveer Bhagod <112793546+K3-github@users.noreply.github.com>
2026-08-05 22:31:55 +0530

    Time: 0 ms (100%), Space: 42.8 MB (95.57%) - LeetHub

--- 0877-stone-game/0877-stone-game.java
+++ 0877-stone-game/0877-stone-game.java
@@ -1,19 +1,5 @@
 class Solution {
-    private int solve(int[] piles, int left, int right,int[][] dp) {
-        if(left>right) return 0;
-        if(dp[left][right]!=-1) return dp[left][right];
-
-        int pickLeft = piles[left] - solve(piles, left + 1, right,dp);
-        int pickRight = piles[right] - solve(piles, left, right - 1,dp);
-
-        return dp[left][right]=Math.max(pickLeft, pickRight);
-    }
     public boolean stoneGame(int[] piles) {
-        int n=piles.length;
-        int[][] dp=new int[n][n];
-        for(int i=0;i<n;i++){
-            Arrays.fill(dp[i],-1);
-        }
-        return solve(piles, 0, piles.length - 1,dp) >= 0;
+        return true;
     }
 }
