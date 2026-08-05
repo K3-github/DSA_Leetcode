3c7ab06ae4c3a2dc2cc1feae7bfd85b4bd9f834f
Krishnveer Bhagod <112793546+K3-github@users.noreply.github.com>
2026-08-06 00:17:32 +0530

    Time: 9 ms (35.14%), Space: 44.4 MB (32.23%) - LeetHub

--- 1012-numbers-with-repeated-digits/1012-numbers-with-repeated-digits.java
+++ 1012-numbers-with-repeated-digits/1012-numbers-with-repeated-digits.java
@@ -1,8 +1,9 @@
 class Solution {
     int[] digits;
     int len;
-    private int solve(int pos,boolean tight,int mask){
+    private int solve(int pos,boolean tight,int mask,int[][] dp){
         if(pos==len) return mask==0 ? 0 : 1;
+        if(!tight && dp[pos][mask]!=-1) return dp[pos][mask];
 
         int limit=tight ? digits[pos] : 9;
         int ans=0;
@@ -10,15 +11,16 @@ class Solution {
         for(int digit=0;digit<=limit;digit++){
             boolean newTight=tight && (digit==limit);
             if(mask==0 && digit==0){
-                ans+=solve(pos+1,newTight,mask);
+                ans+=solve(pos+1,newTight,mask,dp);
             }
             else{
                 if((mask & (1<<digit))!=0) continue;
 
                 int newMask=mask | (1<<digit);
-                ans+=solve(pos+1,newTight,newMask);
+                ans+=solve(pos+1,newTight,newMask,dp);
             }
         }
+        if(!tight) return dp[pos][mask]=ans;
         return ans;
     }
     public int numDupDigitsAtMostN(int n) {
@@ -28,6 +30,10 @@ class Solution {
         for(int i=0;i<len;i++){
             digits[i]=str.charAt(i)-'0';
         }
-        return n-solve(0,true,0);
+        int[][] dp=new int[len][1<<10];
+        for(int i=0;i<len;i++){
+            Arrays.fill(dp[i],-1);
+        }
+        return n-solve(0,true,0,dp);
     }
 }
