e331530c02509a57f43ef6803956bb8ab7736b3c
Krishnveer Bhagod <112793546+K3-github@users.noreply.github.com>
2026-08-09 03:19:27 +0530

    Time: 11 ms (14.39%), Space: 43.3 MB (78.48%) - LeetHub

---
+++ 0010-regular-expression-matching/0010-regular-expression-matching.java
@@ -0,0 +1,31 @@
+class Solution {
+    private boolean solve(int i,int j,String s,String p){
+        if(j<0) return i<0;
+        if(i<0){
+            if(j>=1 && p.charAt(j)=='*') return solve(i,j-2,s,p);
+            return false;
+        }
+
+        char ch1=s.charAt(i);
+        char ch2=p.charAt(j);
+
+        if(ch1==ch2 || ch2=='.'){
+            return solve(i-1,j-1,s,p);
+        }
+        if(ch2=='*'){
+            boolean zero = solve(i, j - 2, s, p);
+            boolean oneOrMore = false;
+            char prev = p.charAt(j-1);
+            if(prev == '.' || prev == ch1) {
+                oneOrMore = solve(i - 1, j, s, p);
+            }
+            return zero || oneOrMore;
+        }
+        return false;
+    }
+    public boolean isMatch(String s, String p) {
+        int n=s.length();
+        int m=p.length();
+        return solve(n-1,m-1,s,p);
+    }
+}
