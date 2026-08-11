2c09f9feb51c60bd8687c4b84951ea21081d47e8
Krishnveer Bhagod <112793546+K3-github@users.noreply.github.com>
2026-08-11 22:45:14 +0530

    Time: 7 ms (40.32%), Space: 76 MB (49.13%) - LeetHub

--- 1664-ways-to-make-a-fair-array/1664-ways-to-make-a-fair-array.java
+++ 1664-ways-to-make-a-fair-array/1664-ways-to-make-a-fair-array.java
@@ -1,28 +1,20 @@
 class Solution {
     public int waysToMakeFair(int[] nums) {
+        int te=0,to=0;
         int n=nums.length;
-        int[] podd=new int[n+1];
-        int[] peven=new int[n+1];
-        
         for(int i=0;i<n;i++){
-            peven[i+1]=peven[i];
-            podd[i+1]=podd[i];
-            if(i%2==0){
-                peven[i+1]=peven[i]+nums[i];
-            }
-            else{
-                podd[i+1]=podd[i]+nums[i];
-            }
+            if(i%2==0) te+=nums[i];
+            else to+=nums[i];
         }
-        int even=0,odd=0;
+        int e=0,o=0;
         int ans=0;
-        for(int i=n-1;i>=0;i--){
-             int teven=peven[i]+odd;
-             int todd=podd[i]+even;
-             if(teven==todd) ans++;
+        for(int i=0;i<n;i++){
+            int re=te-e-(i%2==0 ? nums[i] : 0);
+            int ro=to-o-(i%2==1 ? nums[i] : 0);
 
-             if(i%2==0) even+=nums[i];
-             else odd+=nums[i];
+            if(e+ro==o+re) ans++;
+            if(i%2==0) e+=nums[i];
+            else o+=nums[i];
         }
         return ans;
     }
