2c06bb2426bbca23e2a8e07c716537c8ec0db5de
Krishnveer Bhagod <112793546+K3-github@users.noreply.github.com>
2026-08-12 02:39:44 +0530

    Time: 0 ms (100%), Space: 42.8 MB (43.38%) - LeetHub

--- 1863-sum-of-all-subset-xor-totals/1863-sum-of-all-subset-xor-totals.java
+++ 1863-sum-of-all-subset-xor-totals/1863-sum-of-all-subset-xor-totals.java
@@ -1,15 +1,10 @@
 class Solution {
     int ans=0;
-    private void solve(int ind,int xor,int[] nums){
-        if(ind>=nums.length){
-            ans+=xor;
-            return;
-        }
-        solve(ind+1,nums[ind]^xor,nums);
-        solve(ind+1,xor,nums);
+    private int solve(int ind,int xor,int[] nums){
+        if(ind>=nums.length) return xor;
+        return solve(ind+1,nums[ind]^xor,nums)+solve(ind+1,xor,nums);
     }
     public int subsetXORSum(int[] nums) {
-        solve(0,0,nums);
-        return ans;
+        return solve(0,0,nums);
     }
 }
