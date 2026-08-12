2f24315b98b8fb012c8553d0a7ab611ea41bae00
Krishnveer Bhagod <112793546+K3-github@users.noreply.github.com>
2026-08-13 01:05:54 +0530

    Time: 0 ms (100%), Space: 43.1 MB (84.86%) - LeetHub

--- 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.java
+++ 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.java
@@ -1,26 +1,30 @@
 class Solution {
-    int[][] dp;
-    private int solve(int ind,int pass,int[] days,int[] costs){
+    int[] dp;
+    private int solve(int ind,int[] days,int[] costs){
+        int n=days.length;
         if(ind>=days.length) return 0;
-        if(dp[ind][pass]!=-1) return dp[ind][pass];
-        int ans=4000000;
-        if(pass<days[ind]){
-          int w1=costs[0]+solve(ind+1,days[ind],days,costs);
-          int w2=costs[1]+solve(ind+1,days[ind]+6,days,costs);
-          int w3=costs[2]+solve(ind+1,days[ind]+29,days,costs);
-          ans=Math.min(w1,Math.min(w2,w3));
+        if(dp[ind]!=-1) return dp[ind];
+        int i=ind;
+        while(i<n && days[i]<days[ind]+1){
+            i++;
         }
-        if(pass >= days[ind]){
-            return dp[ind][pass]=solve(ind + 1, pass, days, costs);
+        int w1=costs[0]+solve(i,days,costs);
+        i=ind;
+        while(i<n && days[i]<days[ind]+7){
+            i++;
         }
-        return dp[ind][pass]=ans;
+        int w2=costs[1]+solve(i,days,costs);
+        i=ind;
+        while(i<n && days[i]<days[ind]+30){
+            i++;
+        }
+        int w3=costs[2]+solve(i,days,costs);
+        return dp[ind]=Math.min(w1,Math.min(w2,w3));
     }
     public int mincostTickets(int[] days, int[] costs) {
         int n=days.length;
-        dp=new int[n][365+30+1];
-        for(int i=0;i<n;i++){
-            Arrays.fill(dp[i],-1);
-        }
-        return solve(0,0,days,costs);
+        dp=new int[n];
+        Arrays.fill(dp,-1);
+        return solve(0,days,costs);
     }
 }
