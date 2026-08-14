551aa527c70e733b2b504aa6e8d31c3e823e47eb
Krishnveer Bhagod <112793546+K3-github@users.noreply.github.com>
2026-08-15 00:23:04 +0530

    Time: 2156 ms (5.33%), Space: 46.9 MB (55.06%) - LeetHub

--- 0454-4sum-ii/0454-4sum-ii.java
+++ 0454-4sum-ii/0454-4sum-ii.java
@@ -1,51 +1,35 @@
 class Solution {
     public int fourSumCount(int[] nums1, int[] nums2, int[] nums3, int[] nums4) {
-        int n = nums1.length;
-        int m = nums2.length;
-        int p = nums3.length;
-        int q = nums4.length;
-        int ans = 0;
-
+        int n=nums1.length;
+        int m=nums2.length;
+        int p=nums3.length;
+        int q=nums4.length;
+        int ans=0;
         Arrays.sort(nums3);
         Arrays.sort(nums4);
-
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < m; j++) {
-                int target = -(nums1[i] + nums2[j]);
-
-                int left = 0;
-                int right = q - 1;
-
-                while(left < p && right >= 0) {
-                    int sum = nums3[left] + nums4[right];
-
-                    if(sum == target) {
-                        int noLeft = 1;
-                        while(left + 1 < p && nums3[left] == nums3[left + 1]) {
-                            noLeft++;
-                            left++;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                int target=0-nums1[i]-nums2[j];
+                int left=0,right=q-1;
+                while(left<p && right>=0){
+                    int sum=nums3[left]+nums4[right];
+                    if(sum==target){
+                        int noLeft=1;
+                        while(left<p-1 && nums3[left]==nums3[left+1]){
+                            noLeft++;left++;
                         }
-
-                        int noRight = 1;
-                        while(right - 1 >= 0 && nums4[right] == nums4[right - 1]) {
-                            noRight++;
-                            right--;
+                        int noRight=1;
+                        while(right>0 && nums4[right]==nums4[right-1]){
+                            noRight++;right--;
                         }
-
-                        ans += noLeft * noRight;
-                        left++;
-                        right--;
+                        ans+=noLeft*noRight;
+                        left++;right--;
                     }
-                    else if(sum < target) {
-                        left++;
-                    }
-                    else {
-                        right--;
-                    }
+                    else if(sum<target) left++;
+                    else right--;
                 }
             }
         }
-
         return ans;
     }
 }
