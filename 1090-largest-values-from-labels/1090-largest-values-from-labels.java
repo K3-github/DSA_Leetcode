8349a92ec8ff7be99dfd156a31886c677a7610de
Krishnveer Bhagod <112793546+K3-github@users.noreply.github.com>
2026-09-04 00:41:32 +0530

    Time: 18 ms (76.83%), Space: 49.1 MB (8.49%) - LeetHub

--- 1090-largest-values-from-labels/1090-largest-values-from-labels.java
+++ 1090-largest-values-from-labels/1090-largest-values-from-labels.java
@@ -5,16 +5,17 @@ class Solution {
         for(int i=0;i<n;i++){
             vec[i]=new int[]{values[i],labels[i]};
         }
-        Arrays.sort(vec, (a, b) -> Integer.compare(b[0], a[0]));
+        Arrays.sort(vec, (a, b) -> Integer.compare(a[0], b[0]));
         HashMap<Integer,Integer> mp=new HashMap<>();
         int ans=0;
-        for(int i = 0; i < n && numWanted > 0; i++) {
-            int label = vec[i][1];
-            int count = mp.getOrDefault(label, 0);
-            if(count < useLimit) {
-                ans += vec[i][0];
+        for(int i=n-1;i>=0;i--){
+            int label=vec[i][1];
+            int count=mp.getOrDefault(vec[i][1],0);
+            if(count<useLimit){
+                if(numWanted>0) ans+=vec[i][0];
+                else return ans;
                 numWanted--;
-                mp.put(label, count + 1);
+                mp.put(label,count+1);
             }
         }
         return ans;
